6c9edb3757857f81ad381a6b6862becaa365b41e
Fuxing Loh <git@fuxing.dev>
2021-04-21 22:50:27 +0800

    added bootstrapped files for reference

---
+++ whale-server/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+async function bootstrap (): Promise<void> {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
+}
+
+/* eslint-disable no-void */
+void bootstrap()
