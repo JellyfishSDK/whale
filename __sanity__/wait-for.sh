6742aef0e61ad628e65ff9ae043dc47c60905218
Fuxing Loh <git@fuxing.dev>
2021-04-27 01:41:01 +0800

    setup sanity testing

---
+++ __sanity__/wait-for.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+TIMEOUT=30
+
+wait_for() {
+  for _ in $(seq $TIMEOUT); do
+    result=$(curl "$1" -o /dev/null -w '%{http_code}\n' -s)
+
+    if [ "$result" -eq "200" ]; then
+      echo "$1 is up"
+      return
+    fi
+    sleep 5
+    echo "Waiting for $1 to be up..."
+
+  done
+  echo "Wait for timed out" >&2
+  exit 1
+}
