147b2e0b5538543f55e45db66b9ef4f7c70029ea
websterlcl <w.canonbrother@gmail.com>
2021-05-07 17:42:02 +0800

    remove validation.exception

--- src/module.api/exceptions/index.ts
+++ src/module.api/exceptions/index.ts
@@ -1,2 +1,1 @@
 export * from './bad-request.exception'
-export * from './validation.exception'
--- src/module.api/exceptions/validation.exception.ts
+++
@@ -1,9 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common'
-import { VALIDATION_ERROR } from '../constants'
-import { AppErrorMessage } from '../interfaces'
-
-export class ValidationError extends HttpException {
-  constructor (error?: AppErrorMessage) {
-    super(error ?? VALIDATION_ERROR.message, HttpStatus.BAD_REQUEST)
-  }
-}
