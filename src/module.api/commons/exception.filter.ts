d5aae38709cc78b6c4e5819a57e68af7d7767610
websterlcl <w.canonbrother@gmail.com>
2021-05-09 19:24:54 +0800

    fix codeclimate

--- src/module.api/commons/exception.filter.ts
+++ src/module.api/commons/exception.filter.ts
@@ -1,8 +1,9 @@
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common'
+import { ApiError } from './error.exception'
 
 interface ErrorMessage {
   message: string
-  statusCode: number
+  code: number
   error?: any
   url: string
   at: string
@@ -10,39 +11,28 @@ interface ErrorMessage {
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
+  catch (exception: ApiError, host: ArgumentsHost): any
+
   catch (exception: HttpException, host: ArgumentsHost): any
 
   catch (exception: unknown, host: ArgumentsHost): any
 
-  catch (exception: HttpException | unknown, host: ArgumentsHost): any {
+  catch (exception: ApiError | HttpException | unknown, host: ArgumentsHost): any {
     const request = host.switchToHttp().getRequest().raw
     const response = host.switchToHttp().getResponse()
 
-    if (exception instanceof HttpException) {
+    if (exception instanceof ApiError || exception instanceof HttpException) {
       const status = exception.getStatus()
 
-      if (typeof exception.getResponse() === 'string') {
-        const message = exception.getResponse() as string
-        const errorMessage = {
-          message,
-          code: status,
-          url: request.url,
-          at: new Date().toISOString()
-        }
+      const exceptionResponse = exception.getResponse()
 
-        return response.code(status).send(errorMessage)
-      }
-
-      const exceptionResponse = exception.getResponse() as ErrorMessage
-      const errorMessage = {
-        message: exceptionResponse.message,
-        error: exceptionResponse.error,
+      return response.code(status).send({
+        message: (exceptionResponse as ErrorMessage)?.message ?? exceptionResponse,
+        error: (exceptionResponse as ErrorMessage)?.error,
         code: status,
         url: request.url,
         at: new Date().toISOString()
-      }
-
-      return response.code(status).send(errorMessage)
+      })
     }
 
     return response.code(status).send({
