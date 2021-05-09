a4531f59bf8b96869e74c6cf4ef47f38b2e5af8c
websterlcl <w.canonbrother@gmail.com>
2021-05-09 19:56:10 +0800

    remove validationerr

--- src/module.api/commons/error.exception.ts
+++ src/module.api/commons/error.exception.ts
@@ -10,11 +10,3 @@ export class BadRequestError extends ApiError {
     super(message ?? BadRequestError.DEFAULT_MESSAGE, HttpStatus.BAD_REQUEST)
   }
 }
-
-export class ValidationError extends ApiError {
-  static DEFAULT_MESSAGE = 'Request body contains error.'
-
-  constructor (message?: string) {
-    super(message ?? ValidationError.DEFAULT_MESSAGE, HttpStatus.BAD_REQUEST)
-  }
-}
