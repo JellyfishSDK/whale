3e35eb7e0646a2757bdb2656c3ed10d815f9db87
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-05-11 10:01:29 +0800

    error handling specification (#58)
    * added exception.filter, err.exception and refactors
    
    * finallize constant error message and fix tx.spec
    
    * code consistent
    
    * fix test
    
    * remove validation.exception
    
    * fix rq: refactor
    
    * fix codeclimate
    
    * remove validationerr
    
    * renamed index.ts to module.ts
    moved guards/interceptors into their own folder
    
    * cleaned up error handling specification
    
    Co-authored-by: websterlcl <w.canonbrother@gmail.com>

--- __e2e__/call.e2e.ts
+++ __e2e__/call.e2e.ts
@@ -24,9 +24,13 @@ it('should 404 with invalid network', async () => {
 
   expect(res.statusCode).toBe(404)
   expect(res.json()).toEqual({
-    error: 'Not Found',
-    message: 'Network not found',
-    statusCode: 404
+    error: {
+      code: 404,
+      type: 'NotFound',
+      at: expect.any(Number),
+      message: 'Network not found',
+      url: '/v1/mainnet/call/getblockchaininfo'
+    }
   })
 })
 
@@ -38,9 +42,13 @@ it('should 403 with non whitelisted method', async () => {
 
   expect(res.statusCode).toBe(403)
   expect(res.json()).toEqual({
-    error: 'Forbidden',
-    message: 'RPC method not whitelisted',
-    statusCode: 403
+    error: {
+      code: 403,
+      type: 'Forbidden',
+      message: 'RPC method not whitelisted',
+      at: expect.any(Number),
+      url: '/v1/regtest/call/getbalance'
+    }
   })
 })
 
@@ -57,9 +65,13 @@ it('should 400 with invalid post body params', async () => {
 
   expect(res.statusCode).toBe(400)
   expect(res.json()).toEqual({
-    error: 'Bad Request',
-    message: "RpcApiError: 'Unknown named parameter block', code: -8",
-    statusCode: 400
+    error: {
+      code: 400,
+      type: 'BadRequest',
+      message: "RpcApiError: 'Unknown named parameter block', code: -8",
+      at: expect.any(Number),
+      url: '/v1/regtest/call/getblock'
+    }
   })
 })
 
