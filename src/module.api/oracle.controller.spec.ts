8e1413447c960a19caaade105a918d33b519956e
jingyi2811 <jingyi2811@gmail.com>
2021-06-15 15:13:30 +0800

    Add 13 tests for controller

--- src/module.api/oracle.controller.spec.ts
+++ src/module.api/oracle.controller.spec.ts
@@ -1,43 +1,356 @@
-// import { Test, TestingModule } from '@nestjs/testing'
-// import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
-// import { JsonRpcClient } from '@defichain/jellyfish-api-jsonrpc'
-// import { OracleController } from '@src/module.api/oracle.controller'
-//
-// const container = new MasterNodeRegTestContainer()
-// let client: JsonRpcClient
-// let controller: OracleController
-//
-// beforeAll(async () => {
-//   await container.start()
-//   await container.waitForReady()
-//   await container.waitForWalletCoinbaseMaturity()
-//   client = new JsonRpcClient(await container.getCachedRpcUrl())
-//
-//   const app: TestingModule = await Test.createTestingModule({
-//     controllers: [OracleController],
-//     providers: [{ provide: JsonRpcClient, useValue: client }]
-//   }).compile()
-//   controller = app.get<OracleController>(OracleController)
-// })
-//
-// afterAll(async () => {
-//   await container.stop()
-// })
-//
-// describe('list', () => {
-//   it('should list', async () => {
-//
-//   })
-//
-//   it('should list with pagination', async () => {
-//
-//   })
-//
-//   it('should list empty object as out of range', async () => {
-//
-//   })
-// })
-//
-// describe('get', () => {
-//
-// })
+import { Test, TestingModule } from '@nestjs/testing'
+import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
+import { JsonRpcClient } from '@defichain/jellyfish-api-jsonrpc'
+import { OracleController, OracleRawPriceState } from '@src/module.api/oracle.controller'
+import { NotFoundException } from '@nestjs/common'
+import BigNumber from 'bignumber.js'
+
+const container = new MasterNodeRegTestContainer()
+let client: JsonRpcClient
+let controller: OracleController
+
+beforeAll(async () => {
+  await container.start()
+  await container.waitForReady()
+  await container.waitForWalletCoinbaseMaturity()
+  client = new JsonRpcClient(await container.getCachedRpcUrl())
+
+  const app: TestingModule = await Test.createTestingModule({
+    controllers: [OracleController],
+    providers: [{ provide: JsonRpcClient, useValue: client }]
+  }).compile()
+  controller = app.get<OracleController>(OracleController)
+  await setup()
+})
+
+let oracleid1: string
+let oracleid2: string
+let oracleid3: string
+let oracleid4: string
+let oracleid5: string
+let oracleid6: string
+let oracleid7: string
+let oracleid8: string
+let timestamp: number
+
+async function setup (): Promise<void> {
+  const priceFeeds1 = [
+    { token: 'APPLE', currency: 'EUR' }
+  ]
+
+  const priceFeeds2 = [
+    { token: 'TESLA', currency: 'USD' }
+  ]
+
+  const priceFeeds3 = [
+    { token: 'FB', currency: 'CNY' }
+  ]
+
+  const priceFeeds4 = [
+    { token: 'MFST', currency: 'SGD' }
+  ]
+
+  oracleid1 = await container.call('appointoracle', [await container.getNewAddress(), priceFeeds1, 1])
+  oracleid2 = await container.call('appointoracle', [await container.getNewAddress(), priceFeeds1, 2])
+  oracleid3 = await container.call('appointoracle', [await container.getNewAddress(), priceFeeds2, 3])
+  oracleid4 = await container.call('appointoracle', [await container.getNewAddress(), priceFeeds2, 4])
+  oracleid5 = await container.call('appointoracle', [await container.getNewAddress(), priceFeeds3, 5])
+  oracleid6 = await container.call('appointoracle', [await container.getNewAddress(), priceFeeds3, 6])
+  oracleid7 = await container.call('appointoracle', [await container.getNewAddress(), priceFeeds4, 7])
+  oracleid8 = await container.call('appointoracle', [await container.getNewAddress(), priceFeeds4, 8])
+
+  await container.generate(1)
+
+  timestamp = Math.floor(new Date().getTime() / 1000)
+
+  const prices1 = [{ tokenAmount: '0.5@APPLE', currency: 'EUR' }]
+  await client.oracle.setOracleData(oracleid1, timestamp, { prices: prices1 })
+
+  const prices2 = [{ tokenAmount: '1.0@APPLE', currency: 'EUR' }]
+  await client.oracle.setOracleData(oracleid2, timestamp, { prices: prices2 })
+
+  const prices3 = [{ tokenAmount: '1.5@TESLA', currency: 'USD' }]
+  await client.oracle.setOracleData(oracleid3, timestamp, { prices: prices3 })
+
+  const prices4 = [{ tokenAmount: '2.0@TESLA', currency: 'USD' }]
+  await client.oracle.setOracleData(oracleid4, timestamp, { prices: prices4 })
+
+  const prices5 = [{ tokenAmount: '2.5@FB', currency: 'CNY' }]
+  await client.oracle.setOracleData(oracleid5, timestamp, { prices: prices5 })
+
+  const prices6 = [{ tokenAmount: '3.0@FB', currency: 'CNY' }]
+  await client.oracle.setOracleData(oracleid6, timestamp, { prices: prices6 })
+
+  const prices7 = [{ tokenAmount: '3.5@MFST', currency: 'SGD' }]
+  await client.oracle.setOracleData(oracleid7, timestamp, { prices: prices7 })
+
+  const prices8 = [{ tokenAmount: '4.0@MFST', currency: 'SGD' }]
+  await client.oracle.setOracleData(oracleid8, timestamp, { prices: prices8 })
+
+  await container.generate(1)
+}
+
+afterAll(async () => {
+  await container.stop()
+})
+
+describe('list', () => {
+  it('should list', async () => {
+    const result = await controller.list({ size: 2 })
+    expect(result.data.length).toStrictEqual(2)
+
+    expect(result.data[0]).toStrictEqual(
+      {
+        weightage: 1,
+        oracleid: oracleid1,
+        address: expect.any(String),
+        priceFeeds: [
+          { token: 'APPLE', currency: 'EUR' }
+        ],
+        tokenPrices: [
+          {
+            amount: 0.5,
+            currency: 'EUR',
+            timestamp,
+            token: 'APPLE'
+          }
+        ]
+      }
+    )
+
+    expect(result.data[1]).toStrictEqual(
+      {
+        weightage: 2,
+        oracleid: oracleid2,
+        address: expect.any(String),
+        priceFeeds: [
+          { token: 'APPLE', currency: 'EUR' }
+        ],
+        tokenPrices: [
+          {
+            amount: 1.0,
+            currency: 'EUR',
+            timestamp,
+            token: 'APPLE'
+          }
+        ]
+      }
+    )
+  })
+
+  it('should list with pagination', async () => {
+    const first = await controller.list({ size: 4 })
+
+    expect(first.data.length).toStrictEqual(4)
+    expect(first.page?.next).toStrictEqual(oracleid4)
+
+    expect(first.data[0]).toStrictEqual(expect.objectContaining({ oracleid: oracleid1 }))
+    expect(first.data[1]).toStrictEqual(expect.objectContaining({ oracleid: oracleid2 }))
+    expect(first.data[2]).toStrictEqual(expect.objectContaining({ oracleid: oracleid3 }))
+    expect(first.data[3]).toStrictEqual(expect.objectContaining({ oracleid: oracleid4 }))
+
+    const next = await controller.list({
+      size: 4,
+      next: first.page?.next
+    })
+
+    expect(next.data.length).toStrictEqual(4)
+    expect(next.page?.next).toStrictEqual(oracleid8)
+
+    expect(next.data[0]).toStrictEqual(expect.objectContaining({ oracleid: oracleid5 }))
+    expect(next.data[1]).toStrictEqual(expect.objectContaining({ oracleid: oracleid6 }))
+    expect(next.data[2]).toStrictEqual(expect.objectContaining({ oracleid: oracleid7 }))
+    expect(next.data[3]).toStrictEqual(expect.objectContaining({ oracleid: oracleid8 }))
+
+    const last = await controller.list({
+      size: 1,
+      next: next.page?.next
+    })
+
+    expect(last.data.length).toStrictEqual(0)
+    expect(last.page).toBeUndefined()
+  })
+
+  it('should list empty object as out of range', async () => {
+    const result = await controller.list({ size: 100, next: '300' })
+
+    expect(result.data.length).toStrictEqual(0)
+    expect(result.page).toBeUndefined()
+  })
+})
+
+describe('get', () => {
+  it('should get oracle1 with oracleid1 numeric id', async () => {
+    const data = await controller.get(oracleid1)
+    expect(data).toStrictEqual(
+      {
+        weightage: 1,
+        oracleid: oracleid1,
+        address: expect.any(String),
+        priceFeeds: [
+          { token: 'APPLE', currency: 'EUR' }
+        ],
+        tokenPrices: [
+          {
+            amount: 0.5,
+            currency: 'EUR',
+            timestamp,
+            token: 'APPLE'
+          }
+        ]
+      }
+    )
+  })
+
+  it('should throw error while getting non-existent oracle', async () => {
+    expect.assertions(2)
+    try {
+      await controller.get('e40775f8bb396cd3d94429843453e66e68b1c7625d99b0b4c505ab004506697b')
+    } catch (err) {
+      expect(err).toBeInstanceOf(NotFoundException)
+      expect(err.response).toStrictEqual({
+        statusCode: 404,
+        message: 'Unable to find oracle',
+        error: 'Not Found'
+      })
+    }
+  })
+})
+
+describe('rawPrices', () => {
+  it('should getLatestRawPrice', async () => {
+    const result = await controller.listRawPrices({ size: 2 })
+    expect(result.data.length).toStrictEqual(2)
+
+    expect(result.data).toStrictEqual(
+      [
+        {
+          priceFeeds: { token: 'APPLE', currency: 'EUR' },
+          oracleid: oracleid1,
+          weightage: new BigNumber(1),
+          timestamp: new BigNumber(timestamp),
+          rawprice: new BigNumber(0.5),
+          state: OracleRawPriceState.LIVE
+        },
+        {
+          priceFeeds: { token: 'APPLE', currency: 'EUR' },
+          oracleid: oracleid2,
+          weightage: new BigNumber(2),
+          timestamp: new BigNumber(timestamp),
+          rawprice: new BigNumber(1),
+          state: OracleRawPriceState.LIVE
+        }
+      ]
+    )
+  })
+
+  it('should getLatestRawPrice with pagination', async () => {
+    const first = await controller.list({ size: 4 })
+
+    expect(first.data.length).toStrictEqual(4)
+    expect(first.page?.next).toStrictEqual(oracleid4)
+
+    expect(first.data[0]).toStrictEqual(expect.objectContaining({ oracleid: oracleid1 }))
+    expect(first.data[1]).toStrictEqual(expect.objectContaining({ oracleid: oracleid2 }))
+    expect(first.data[2]).toStrictEqual(expect.objectContaining({ oracleid: oracleid3 }))
+    expect(first.data[3]).toStrictEqual(expect.objectContaining({ oracleid: oracleid4 }))
+
+    const next = await controller.list({
+      size: 4,
+      next: first.page?.next
+    })
+
+    expect(next.data.length).toStrictEqual(4)
+    expect(next.page?.next).toStrictEqual(oracleid8)
+
+    expect(next.data[0]).toStrictEqual(expect.objectContaining({ oracleid: oracleid5 }))
+    expect(next.data[1]).toStrictEqual(expect.objectContaining({ oracleid: oracleid6 }))
+    expect(next.data[2]).toStrictEqual(expect.objectContaining({ oracleid: oracleid7 }))
+    expect(next.data[3]).toStrictEqual(expect.objectContaining({ oracleid: oracleid8 }))
+
+    const last = await controller.list({
+      size: 1,
+      next: next.page?.next
+    })
+
+    expect(last.data.length).toStrictEqual(0)
+    expect(last.page).toBeUndefined()
+  })
+
+  it('should getLatestRawPrice with empty object as out of range', async () => {
+    const result = await controller.list({ size: 100, next: '300' })
+
+    expect(result.data.length).toStrictEqual(0)
+    expect(result.page).toBeUndefined()
+  })
+})
+
+describe('listPrices', () => {
+  it('should listPrices', async () => {
+    const result = await controller.listPrices({ size: 2 })
+    expect(result.data.length).toStrictEqual(2)
+    // NOTE(jingyi2811): 0.83333333000000 = (0.5 * 1 + 1 * 2) / 3
+    // NOTE(jingyi2811): 1.78571428000000 = (1.5 * 3 + 2 * 4) / 7
+    expect(result.data).toStrictEqual([
+      { token: 'APPLE', currency: 'EUR', price: new BigNumber(0.83333333000000), ok: true },
+      { token: 'FB', currency: 'CNY', price: new BigNumber(2.77272727000000), ok: true }
+    ])
+  })
+
+  it('should listPrices with pagination', async () => {
+    const first = await controller.listPrices({ size: 2 })
+
+    expect(first.data.length).toStrictEqual(2)
+    expect(first.page?.next).toStrictEqual('FB-CNY')
+
+    expect(first.data[0]).toStrictEqual(expect.objectContaining({ token: 'APPLE', currency: 'EUR' }))
+    expect(first.data[1]).toStrictEqual(expect.objectContaining({ token: 'FB', currency: 'CNY' }))
+
+    const next = await controller.listPrices({
+      size: 2,
+      next: first.page?.next
+    })
+
+    expect(next.data.length).toStrictEqual(2)
+    expect(next.page?.next).toStrictEqual('TESLA-USD')
+
+    expect(next.data[0]).toStrictEqual(expect.objectContaining({ token: 'MFST', currency: 'SGD' }))
+    expect(next.data[1]).toStrictEqual(expect.objectContaining({ token: 'TESLA', currency: 'USD' }))
+
+    const last = await controller.listPrices({
+      size: 1,
+      next: next.page?.next
+    })
+
+    expect(last.data.length).toStrictEqual(0)
+    expect(last.page).toBeUndefined()
+  })
+
+  it('should listPrices with empty object as out of range', async () => {
+    const result = await controller.listPrices({ size: 100, next: '300' })
+
+    expect(result.data.length).toStrictEqual(0)
+    expect(result.page).toBeUndefined()
+  })
+})
+
+describe('getPrice', () => {
+  it('should get APPLE / USD price with APPLE token and EUR currency', async () => {
+    const data = await controller.getPrice('APPLE', 'EUR')
+    expect(data.toString()).toStrictEqual(new BigNumber('0.83333333').toString())
+  })
+
+  it('should throw error while getting non-existent oracle', async () => {
+    expect.assertions(2)
+    try {
+      await controller.getPrice('BAIDU', 'MYR')
+    } catch (err) {
+      expect(err).toBeInstanceOf(NotFoundException)
+      expect(err.response).toStrictEqual({
+        statusCode: 404,
+        message: 'Unable to find oracle',
+        error: 'Not Found'
+      })
+    }
+  })
+})
--- src/module.api/oracle.controller.ts
+++ src/module.api/oracle.controller.ts
@@ -4,8 +4,6 @@ import { ApiPagedResponse } from '@src/module.api/_core/api.paged.response'
 import { PaginationQuery } from '@src/module.api/_core/api.query'
 import { OraclePriceFeed } from '@defichain/jellyfish-api-core/dist/category/oracle'
 import BigNumber from 'bignumber.js'
-import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
-const container = new MasterNodeRegTestContainer()
 
 @Controller('/v1/:network/oracle')
 export class OracleController {
@@ -24,7 +22,7 @@ export class OracleController {
   ): Promise<ApiPagedResponse<OracleData>> {
     const oracleids = await this.client.oracle.listOracles()
 
-    let oracles = []
+    let oracles: any[] = []
 
     for (let i = 0; i < oracleids.length; i += 1) {
       const oracleid = oracleids[i]
@@ -32,9 +30,23 @@ export class OracleController {
       oracles.push(oracle)
     }
 
-    oracles = oracles.sort((a: OracleData, b: OracleData) => (a.oracleid > b.oracleid ? 1 : -1))
+    oracles = oracles.sort((a: OracleData, b: OracleData) => (a.weightage > b.weightage ? 1 : -1))
 
-    return ApiPagedResponse.of(oracles, oracles.length, item => {
+    const start = query.next !== undefined ? query.next : oracles[0].oracleid
+
+    let indexStart = oracles.map(x => x.oracleid).indexOf(start)
+
+    if (indexStart > 0 && start !== oracles[0].oracleid) {
+      indexStart += 1
+    }
+
+    if (indexStart >= 0) {
+      oracles = oracles.slice(indexStart, indexStart + query.size)
+    } else {
+      oracles = []
+    }
+
+    return ApiPagedResponse.of(oracles, query.size, item => {
       return item.oracleid
     })
   }
@@ -51,7 +63,8 @@ export class OracleController {
       return await this.client.oracle.getOracleData(oracleid)
     } catch (e) {
       /* istanbul ignore else */
-      if (e.payload.message === 'Oracle not found') {
+      console.log(e.payload.message)
+      if (e.payload.message === `oracle <${oracleid}> not found`) {
         throw new NotFoundException('Unable to find oracle')
       } else {
         throw new BadRequestException(e)
@@ -69,11 +82,25 @@ export class OracleController {
   async listRawPrices (
     @Query() query: PaginationQuery
   ): Promise<ApiPagedResponse<OracleRawPrice>> {
-    // return await this.client.oracle.listLatestRawPrices()
-    const oracles = (await container.call('listlatestrawprices'))
-      .sort((a: OracleData, b: OracleData) => (a.oracleid > b.oracleid ? 1 : -1))
+    let oracles: OracleRawPrice[] = await this.client.call('listlatestrawprices', [], 'bignumber')
 
-    return ApiPagedResponse.of(oracles, oracles.length, item => {
+    oracles = oracles.sort((a: OracleRawPrice, b: OracleRawPrice) => (a.weightage > b.weightage ? 1 : -1))
+
+    const start = query.next !== undefined ? query.next : oracles[0].oracleid
+
+    let indexStart = oracles.map((x: { oracleid: any }) => x.oracleid).indexOf(start)
+
+    if (indexStart > 0 && start !== oracles[0].oracleid) {
+      indexStart += 1
+    }
+
+    if (indexStart >= 0) {
+      oracles = oracles.slice(indexStart, indexStart + query.size)
+    } else {
+      oracles = []
+    }
+
+    return ApiPagedResponse.of(oracles, query.size, item => {
       return item.oracleid
     })
   }
@@ -88,29 +115,43 @@ export class OracleController {
   async listPrices (
     @Query() query: PaginationQuery
   ): Promise<ApiPagedResponse<ListPricesData>> {
-    // return await this.client.oracle.listPrices()
-    const oracles = (await container.call('listprices'))
-      .sort((a: OracleData, b: OracleData) => (a.oracleid > b.oracleid ? 1 : -1))
+    let prices: ListPricesData[] = await this.client.call('listprices', [], 'bignumber')
 
-    return ApiPagedResponse.of(oracles, oracles.length, item => {
-      return item.token
+    prices = prices.sort((a: ListPricesData, b: ListPricesData) => a.token.concat('-').concat(a.currency) > b.token.concat('-').concat(b.currency) ? 1 : -1)
+
+    const start = query.next !== undefined ? query.next : prices[0].token.concat('-').concat(prices[0].currency)
+
+    let indexStart = prices.map(x => x.token.concat('-').concat(x.currency)).indexOf(start)
+
+    if (indexStart > 0 && start !== prices[0].token.concat('-').concat(prices[0].currency)) {
+      indexStart += 1
+    }
+
+    if (indexStart >= 0) {
+      prices = prices.slice(indexStart, indexStart + query.size)
+    } else {
+      prices = []
+    }
+
+    return ApiPagedResponse.of(prices, query.size, item => {
+      return item.token.concat('-').concat(item.currency)
     })
   }
 
   /**
    * Get the oracle price with id of the oracle.
    *
-   * @param {string} oracleid
+   * @param {string} token
+   * @param {string} currency
    * @return {Promise<string>}
    */
   @Get('/price/:id')
-  async getPrice (@Param('id', ParseIntPipe) oracleid: string): Promise<string> {
+  async getPrice (@Param('id', ParseIntPipe) token: string, currency: string): Promise<string> {
     try {
-      // return await this.client.oracle.getPrice(id)
-      return await container.call('getprice', [oracleid])
+      return await this.client.call('getprice', [{ token, currency }], 'bignumber')
     } catch (e) {
       /* istanbul ignore else */
-      if (e.payload.message === 'Oracle not found') {
+      if (e.payload.message === 'no live oracles for specified request') {
         throw new NotFoundException('Unable to find oracle')
       } else {
         throw new BadRequestException(e)
@@ -120,8 +161,8 @@ export class OracleController {
 }
 
 // Once test containers updated and includes all the RPCS, the following code will be removed
-enum OracleRawPriceState {
-  // LIVE = 'live',
+export enum OracleRawPriceState {
+  LIVE = 'live',
   EXPIRED = 'expired'
 }
 
@@ -155,3 +196,31 @@ interface OracleTokenPrice {
   amount: number
   timestamp: number
 }
+
+interface OracleData {
+  oracleid: string
+  address: string
+  priceFeeds: OraclePriceFeed[]
+  tokenPrices: OracleTokenPrice[]
+  weightage: number
+}
+//
+// interface OracleResult {
+//   id: string
+//   oracleid: string
+//   address: string
+//   priceFeeds: OraclePriceFeed[]
+//   tokenPrices: OracleTokenPrice[]
+//   weightage: number
+// }
+//
+// function mapOracleResult (id: string, oracleData: OracleData): OracleResult {
+//   return {
+//     id: id,
+//     oracleid: oracleData.oracleid,
+//     address: oracleData.address,
+//     priceFeeds: oracleData.priceFeeds,
+//     tokenPrices: oracleData.tokenPrices,
+//     weightage: oracleData.weightage
+//   }
+// }
