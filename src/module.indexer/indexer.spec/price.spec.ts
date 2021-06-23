bc632eb9c7d20d982b089b38241b2dcf103b2e7e
jingyi2811 <jingyi2811@gmail.com>
2021-06-23 13:13:02 +0800

    Minor

--- src/module.indexer/indexer.spec/price.spec.ts
+++ src/module.indexer/indexer.spec/price.spec.ts
@@ -1,21 +1,28 @@
 import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { TestingModule } from '@nestjs/testing'
 import { createIndexerTestModule, stopIndexer, waitForHeight } from '@src/module.indexer/indexer.spec/_testing.module'
+import { JsonRpcClient } from '@defichain/jellyfish-api-jsonrpc'
 
 const container = new MasterNodeRegTestContainer()
 let app: TestingModule
+let client: JsonRpcClient
 
 beforeAll(async () => {
-  await container.start()
-  await container.waitForReady()
-  await container.generate(20)
-  await container.waitForWalletCoinbaseMaturity()
+  try {
+    await container.start()
+    await container.waitForReady()
+    await container.generate(20)
+    await container.waitForWalletCoinbaseMaturity()
 
-  app = await createIndexerTestModule(container)
-  await app.init()
+    app = await createIndexerTestModule(container)
+    await app.init()
 
-  // client = new JsonRpcClient(await container.getCachedRpcUrl())
-  await setup()
+    client = new JsonRpcClient(await container.getCachedRpcUrl())
+    await setup()
+  } catch (e) {
+    console.log(2211)
+    console.log(e)
+  }
 })
 
 afterAll(async () => {
@@ -31,24 +38,22 @@ async function setup (): Promise<void> {
     { token: 'APPLE', currency: 'EUR' }
   ]
 
-  const oracleid = await container.call('appointoracle', [await container.getNewAddress(), priceFeeds, 1])
-
-  await container.generate(1)
-
-  // Update oracle
-  const updateOraclePriceFeeds = [
-    { token: 'FB', currency: 'CNY' }
-  ]
-
-  await container.call('updateoracle', [oracleid, await container.getNewAddress(), updateOraclePriceFeeds, 2])
+  const oracleid = await client.oracle.appointOracle(await container.getNewAddress(), priceFeeds, { weightage: 1 })
 
   await container.generate(1)
 
   const timestamp = new Date().getTime()
-  const prices = [{ tokenAmount: '0.5@FB', currency: 'CNY' }]
-  await container.call('setoracledata', [oracleid, timestamp, prices])
+  const prices = [{ tokenAmount: '0.5@APPLE', currency: 'EUR' }]
+  await client.oracle.setOracleData(oracleid, timestamp, { prices })
 
   await container.generate(1)
+
+  await client.oracle.setOracleData(oracleid, timestamp, { prices })
+
+  console.log(oracleid)
+
+  const data = await client.oracle.getOracleData(oracleid)
+  console.log(data)
 }
 
 describe('x', () => {
--- src/module.indexer/model/oracle.price.ts
+++ src/module.indexer/model/oracle.price.ts
@@ -1,98 +1,97 @@
 import { Injectable } from '@nestjs/common'
 import { Indexer, RawBlock } from '@src/module.indexer/model/_abstract'
-// import { VoutFinder } from '@src/module.indexer/model/_vout_finder'
-import { HexEncoder } from '@src/module.model/_hex.encoder'
-import { OraclePriceAggregation, OraclePriceAggregationMapper } from '@src/module.model/oracle.price'
+import { SmartBuffer } from 'smart-buffer'
+import { CSetOracleData } from '@defichain/jellyfish-transaction'
+import { JellyfishJSON } from '@defichain/jellyfish-api-core'
+// import { BufferComposer, ComposableBuffer } from "@defichain/jellyfish-transaction/dist/buffer/buffer_composer";
+// import BigNumber from "bignumber.js";
 
 @Injectable()
-export class OraclePriceAggregationIndexer extends Indexer {
-  constructor (
-    private readonly mapper: OraclePriceAggregationMapper
-    // private readonly voutFinder: VoutFinder
-  ) {
-    super()
-  }
+export class OraclePriceIndexer extends Indexer {
+  // constructor (
+  //   //private readonly mapper: OraclePriceMapper,
+  // ) {
+  //   super()
+  // }
 
   async index (block: RawBlock): Promise<void> {
-    const records: Record<string, OraclePriceAggregation> = {}
-
-    function findOraclePriceAggregation (hex: string, type: string): OraclePriceAggregation {
-      const hid = HexEncoder.asSHA256(hex)
-
-      // if (hex.includes('6a414466547879')) {
-      //   const buffer = SmartBuffer.fromBuffer(Buffer.from('5fe2c6c2e00f7c764d0386945fa61dccf01374b5d819efad5e9911c9f82fc60cefa369327a01000001055445534c41010355534400a3e11100000000', 'hex'))
-      //   const composable: any = new CSetOracleData(buffer)
-      // }
-
-      if (records[hid] === undefined) {
-        records[hid] = OraclePriceAggregationIndexer.newOraclePriceAggregation(
-          block,
-          hex,
-          type,
-          'APPL',
-          'EUR',
-          0
-        )
-      }
-
-      return records[hid]
-    }
+    // const records: Record<string, OraclePrice> = {}
 
     for (const txn of block.tx) {
-      for (const vout of txn.vout) {
-        // const aggregation =
-        findOraclePriceAggregation(
-          vout.scriptPubKey.hex,
-          vout.scriptPubKey.type
-        )
+      try {
+        const buffer = SmartBuffer.fromBuffer(Buffer.from(txn.hex, 'hex'))
+        const composable: any = new CSetOracleData(buffer)
+        console.log(JellyfishJSON.stringify(composable))
+      } catch (e) {
+        console.log(12345)
+        console.log(e)
       }
     }
-
-    for (const aggregation of Object.values(records)) {
-      await this.mapper.put(aggregation)
-    }
   }
+
+  // async index (block: RawBlock): Promise<void> {
+  //   for (const txn of block.tx) {
+  //     for (const vin of txn.vin) {
+  //       const txn = block.tx.find(tx => tx.txid === vin.txid)
+  //       const vout = txn?.vout.find(vout => vout.n === vin.vout)
+  //       //if(vout?.scriptPubKey.hex.includes('6a414466547879')){
+  //       if(vout?.scriptPubKey.hex){
+  //         const buffer = SmartBuffer.fromBuffer(Buffer.from(vout?.scriptPubKey.hex, 'hex'))
+  //         const composable = new CSetOracleData(buffer)
+  //         console.log(composable)
+  //       }
+  //     }
+  //   }
+  // }
 
   async invalidate (block: RawBlock): Promise<void> {
-    const hidList = new Set<string>()
-
-    for (const txn of block.tx) {
-      for (const vout of txn.vout) {
-        hidList.add(HexEncoder.asSHA256(vout.scriptPubKey.hex))
-      }
-    }
-
-    for (const hid of hidList) {
-      await this.mapper.delete(HexEncoder.encodeHeight(block.height) + hid)
-    }
-  }
-
-  static newOraclePriceAggregation (
-    block: RawBlock,
-    hex: string,
-    type: string,
-    token: string,
-    currency: string,
-    timestamp: number
-  ): OraclePriceAggregation {
-    const hid = HexEncoder.asSHA256(hex)
-
-    return {
-      id: HexEncoder.encodeHeight(block.height) + hid,
-      hid: hid,
-      block: {
-        hash: block.hash,
-        height: block.height
-      },
-      script: {
-        type: type,
-        hex: hex
-      },
-      data: {
-        token,
-        currency,
-        timestamp
-      }
-    }
+    // const records: Record<string, OraclePrice> = {}
+    // for (const oraclePrice of Object.values(records)) {
+    //   this.mapper.delete(oraclePrice)
+    // }
   }
 }
+
+// /**
+//  * Composable SetOracleData, C stands for Composable.
+//  * Immutable by design, bi-directional fromBuffer, toBuffer deep composer.
+//  */
+// export class CSetOracleData extends ComposableBuffer<SetOracleData> {
+//   static OP_CODE = 0x79
+//   static OP_NAME = 'OP_DEFI_TX_SET_ORACLE_DATA'
+//
+//   composers (ao: SetOracleData): BufferComposer[] {
+//     return [
+//       ComposableBuffer.hexBEBufferLE(32, () => ao.oracleId, v => ao.oracleId = v),
+//       ComposableBuffer.bigNumberUInt64(() => ao.timestamp, v => ao.timestamp = v),
+//       ComposableBuffer.varUIntArray(() => ao.tokenPrices, v => ao.tokenPrices = v, v => new CTokenPrice(v))
+//     ]
+//   }
+// }
+//
+// export class CTokenPrice extends ComposableBuffer<TokenPrice> {
+//   composers (sb: TokenPrice): BufferComposer[] {
+//     return [
+//       ComposableBuffer.varUIntUtf8BE(() => sb.token, v => sb.token = v),
+//       ComposableBuffer.varUIntArray(() => sb.amount, v => sb.amount = v, v => new CTokenAmount(v))
+//     ]
+//   }
+// }
+//
+// class CTokenAmount extends ComposableBuffer<TokenPrice> {
+//   composers (tp: TokenPrice): BufferComposer[] {
+//     return [
+//       ComposableBuffer.varUIntUtf8BE(() => tp.token, v => tp.token = v),
+//       ComposableBuffer.varUIntUtf8BE(() => tp.currency, v => tp.currency = v),
+//       ComposableBuffer.satoshiAsBigNumber(() => tp.amount, v => tp.amount = v),
+//       ComposableBuffer.satoshiAsBigNumber(() => tp.timestamp, v => tp.timestamp = v)
+//     ]
+//   }
+// }
+//
+// interface TokenPrice {
+//   token: string
+//   currency: string
+//   amount: BigNumber
+//   timestamp: BigNumber
+// }
