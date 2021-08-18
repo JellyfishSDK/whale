6e0e261c471a332653d7698652e08bd127116e25
Ahmed Hilali <monstrobishi@gmail.com>
2021-08-18 14:27:14 +0800

    Improve code coverage

--- src/module.indexer/model/dftx/create.masternode.spec.ts
+++ src/module.indexer/model/dftx/create.masternode.spec.ts
@@ -1,4 +1,4 @@
-import { createTestingApp, DelayedEunosPayaTestContainer, stopTestingApp, waitForIndexedHeight } from '@src/e2e.module'
+import { createTestingApp, DelayedEunosPayaTestContainer, invalidateFromHeight, stopTestingApp, waitForIndexedHeight } from '@src/e2e.module'
 import { NestFastifyApplication } from '@nestjs/platform-fastify'
 import { JsonRpcClient } from '@defichain/jellyfish-api-jsonrpc'
 import { MasternodeMapper } from '@src/module.model/masternode'
@@ -129,5 +129,57 @@ describe('create masternode (post eunos paya)', () => {
     expect(masternode?.creationHeight).toStrictEqual(masternodeRPCInfo.creationHeight)
     expect(masternode?.resignHeight).toStrictEqual(masternodeRPCInfo.resignHeight)
     expect(masternode?.mintedBlocks).toStrictEqual(masternodeRPCInfo.mintedBlocks)
+  })
+})
+
+describe('invalidate', () => {
+  const container = new MasterNodeRegTestContainer()
+  let app: NestFastifyApplication
+  let client: JsonRpcClient
+
+  beforeAll(async () => {
+    await container.start()
+    await container.waitForWalletCoinbaseMaturity()
+
+    app = await createTestingApp(container)
+    client = new JsonRpcClient(await container.getCachedRpcUrl())
+  })
+
+  afterAll(async () => {
+    await stopTestingApp(container, app)
+  })
+
+  it('should create masternode and invalidate', async () => {
+    await container.generate(1)
+
+    const ownerAddress = await client.wallet.getNewAddress()
+    const masternodeId = await client.masternode.createMasternode(ownerAddress)
+    await container.generate(1)
+
+    const height = await client.blockchain.getBlockCount()
+    await container.generate(1)
+    await waitForIndexedHeight(app, height)
+
+    const masternodeMapper = app.get(MasternodeMapper)
+
+    const masternodeRPCInfo: MasternodeInfo =
+      (await client.masternode.getMasternode(masternodeId))[masternodeId]
+    const masternode = await masternodeMapper.get(masternodeId)
+
+    expect(masternode).not.toStrictEqual(undefined)
+    expect(masternode?.operatorAddress).toStrictEqual(masternodeRPCInfo.operatorAuthAddress)
+    expect(masternode?.ownerAddress).toStrictEqual(masternodeRPCInfo.ownerAuthAddress)
+    expect(masternode?.creationHeight).toStrictEqual(masternodeRPCInfo.creationHeight)
+    expect(masternode?.resignHeight).toStrictEqual(masternodeRPCInfo.resignHeight)
+    expect(masternode?.mintedBlocks).toStrictEqual(masternodeRPCInfo.mintedBlocks)
+
+    await invalidateFromHeight(app, container, height)
+    await container.generate(2)
+    await waitForIndexedHeight(app, height)
+
+    {
+      const invalidatedMasternode = await masternodeMapper.get(masternodeId)
+      expect(invalidatedMasternode).toStrictEqual(undefined)
+    }
   })
 })
