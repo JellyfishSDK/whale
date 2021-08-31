920b9e2ca849b7dee8a2d534346a769f25f5e4de
Ahmed Hilali <monstrobishi@gmail.com>
2021-08-31 15:29:15 +0800

    Indexed create token

---
+++ src/module.indexer/model/dftx/create.token.ts
@@ -0,0 +1,55 @@
+import { DfTxIndexer, DfTxTransaction } from '@src/module.indexer/model/dftx/_abstract'
+import { TokenCreate, CTokenCreate } from '@defichain/jellyfish-transaction'
+import { RawBlock } from '@src/module.indexer/model/_abstract'
+import { Injectable, Logger } from '@nestjs/common'
+import { HexEncoder } from '@src/module.model/_hex.encoder'
+import { DCT_ID_START, TokenMapper } from '@src/module.model/token'
+import BigNumber from 'bignumber.js'
+
+@Injectable()
+export class CreateTokenIndexer extends DfTxIndexer<TokenCreate> {
+  OP_CODE: number = CTokenCreate.OP_CODE
+  private readonly logger = new Logger(CreateTokenIndexer.name)
+
+  constructor (
+    private readonly tokenMapper: TokenMapper
+  ) {
+    super()
+  }
+
+  async index (block: RawBlock, txns: Array<DfTxTransaction<TokenCreate>>): Promise<void> {
+    for (const { dftx: { data } } of txns) {
+      const tokenId = await this.getNextTokenID(data.isDAT)
+      await this.tokenMapper.put({
+        id: `${tokenId}`,
+        sort: HexEncoder.encodeHeight(tokenId),
+        symbol: data.symbol,
+        name: data.name,
+        isDAT: data.isDAT,
+        isLPS: false,
+        limit: data.limit.toFixed(8),
+        mintable: data.mintable,
+        decimal: data.decimal,
+        tradeable: data.tradeable,
+        block: { hash: block.hash, height: block.height, medianTime: block.mediantime, time: block.time }
+      })
+    }
+  }
+
+  async invalidate (_: RawBlock, txns: Array<DfTxTransaction<TokenCreate>>): Promise<void> {
+    for (const { dftx: { data } } of txns) {
+      const tokenId = await this.getNextTokenID(data.isDAT)
+      await this.tokenMapper.delete(`${tokenId - 1}`)
+    }
+  }
+
+  async getNextTokenID (isDAT: boolean): Promise<number> {
+    const latest = isDAT ? await this.tokenMapper.getLatestDAT()
+      : await this.tokenMapper.getLatestDST()
+
+    // Default to 1 as 0 is reserved for DFI
+    const startId = isDAT ? 1 : DCT_ID_START
+
+    return latest !== undefined ? new BigNumber(latest.id).plus(1).toNumber() : startId
+  }
+}
