f6acdc8cf1b4b5f325ec2b497df2dbd6f76b0ec2
jingyi2811 <jingyi2811@gmail.com>
2021-10-05 12:39:34 +0800

    Add collateralToken endpoints

---
+++ packages/whale-api-client/__tests__/api/loan.collateral.test.ts
@@ -0,0 +1,186 @@
+import { StubWhaleApiClient } from '../stub.client'
+import { StubService } from '../stub.service'
+import { WhaleApiClient, WhaleApiException } from '../../src'
+import BigNumber from 'bignumber.js'
+import { Testing } from '@defichain/jellyfish-testing'
+import { LoanMasterNodeRegTestContainer } from './loan_container'
+
+let container: LoanMasterNodeRegTestContainer
+let service: StubService
+let client: WhaleApiClient
+
+let collateralTokenId1: string
+let collateralTokenId2: string
+let collateralTokenId3: string
+let collateralTokenId4: string
+
+beforeAll(async () => {
+  container = new LoanMasterNodeRegTestContainer()
+  service = new StubService(container)
+  client = new StubWhaleApiClient(service)
+
+  await container.start()
+  await container.waitForWalletCoinbaseMaturity()
+  await service.start()
+
+  const testing = Testing.create(container)
+
+  await testing.token.create({ symbol: 'AAPL' })
+  await testing.generate(1)
+
+  await testing.token.create({ symbol: 'TSLA' })
+  await testing.generate(1)
+
+  await testing.token.create({ symbol: 'MSFT' })
+  await testing.generate(1)
+
+  await testing.token.create({ symbol: 'FB' })
+  await testing.generate(1)
+
+  await testing.rpc.oracle.appointOracle(await container.getNewAddress(),
+    [
+      { token: 'AAPL', currency: 'USD' },
+      { token: 'TSLA', currency: 'USD' },
+      { token: 'MSFT', currency: 'USD' },
+      { token: 'FB', currency: 'USD' }
+    ], { weightage: 1 })
+  await testing.generate(1)
+
+  collateralTokenId1 = await testing.rpc.loan.setCollateralToken({
+    token: 'AAPL',
+    factor: new BigNumber(0.1),
+    priceFeedId: 'AAPL/USD'
+  })
+  await testing.generate(1)
+
+  collateralTokenId2 = await testing.rpc.loan.setCollateralToken({
+    token: 'TSLA',
+    factor: new BigNumber(0.2),
+    priceFeedId: 'TSLA/USD'
+  })
+  await testing.generate(1)
+
+  collateralTokenId3 = await testing.rpc.loan.setCollateralToken({
+    token: 'MSFT',
+    factor: new BigNumber(0.3),
+    priceFeedId: 'MSFT/USD'
+  })
+  await testing.generate(1)
+
+  collateralTokenId4 = await testing.rpc.loan.setCollateralToken({
+    token: 'FB',
+    factor: new BigNumber(0.4),
+    priceFeedId: 'FB/USD'
+  })
+  await testing.generate(1)
+})
+
+afterAll(async () => {
+  try {
+    await service.stop()
+  } finally {
+    await container.stop()
+  }
+})
+
+describe('list', () => {
+  it('should listCollateralTokens', async () => {
+    const result = await client.loan.listCollateralTokens()
+    expect(result.length).toStrictEqual(4)
+    expect(result[0]).toStrictEqual(
+      {
+        id: collateralTokenId1,
+        token: 'AAPL',
+        priceFeedId: 'AAPL/USD',
+        factor: 0.1,
+        activateAfterBlock: 107
+      }
+    )
+
+    expect(result[1]).toStrictEqual(
+      {
+        id: collateralTokenId4,
+        token: 'FB',
+        priceFeedId: 'FB/USD',
+        factor: 0.4,
+        activateAfterBlock: 110
+      }
+    )
+
+    expect(result[2]).toStrictEqual(
+      {
+        id: collateralTokenId3,
+        token: 'MSFT',
+        priceFeedId: 'MSFT/USD',
+        factor: 0.3,
+        activateAfterBlock: 109
+      }
+    )
+
+    expect(result[3]).toStrictEqual(
+      {
+        id: collateralTokenId2,
+        token: 'TSLA',
+        priceFeedId: 'TSLA/USD',
+        factor: 0.2,
+        activateAfterBlock: 108
+      }
+    )
+  })
+
+  it('should listLoanSchemes with pagination', async () => {
+    const first = await client.loan.listCollateralTokens(2)
+
+    expect(first.length).toStrictEqual(2)
+    expect(first.hasNext).toStrictEqual(true)
+    expect(first.nextToken).toStrictEqual('FB')
+
+    expect(first[0].token).toStrictEqual('AAPL')
+    expect(first[1].token).toStrictEqual('FB')
+
+    const next = await client.paginate(first)
+
+    expect(next.length).toStrictEqual(2)
+    expect(next.hasNext).toStrictEqual(true)
+    expect(next.nextToken).toStrictEqual('TSLA')
+
+    expect(next[0].token).toStrictEqual('MSFT')
+    expect(next[1].token).toStrictEqual('TSLA')
+
+    const last = await client.paginate(next)
+
+    expect(last.length).toStrictEqual(0)
+    expect(last.hasNext).toStrictEqual(false)
+    expect(last.nextToken).toBeUndefined()
+  })
+})
+
+describe('get', () => {
+  it('should get scheme by scheme id', async () => {
+    const data = await client.loan.getCollateralToken('AAPL')
+    expect(data).toStrictEqual({
+      [collateralTokenId1]: {
+        token: 'AAPL',
+        factor: 0.1,
+        priceFeedId: 'AAPL/USD',
+        activateAfterBlock: 107
+      }
+    })
+  })
+
+  it('should fail due to getting non-existent or malformed id', async () => {
+    expect.assertions(2)
+    try {
+      await client.loan.getCollateralToken('999')
+    } catch (err) {
+      expect(err).toBeInstanceOf(WhaleApiException)
+      expect(err.error).toStrictEqual({
+        code: 404,
+        type: 'NotFound',
+        at: expect.any(Number),
+        message: 'Unable to find collateral token',
+        url: '/v0.0/regtest/loan/collaterals/999'
+      })
+    }
+  })
+})
---
+++ src/module.api/loan.collateral.e2e.ts
@@ -0,0 +1,180 @@
+import { NestFastifyApplication } from '@nestjs/platform-fastify'
+import { createTestingApp, stopTestingApp } from '@src/e2e.module'
+import BigNumber from 'bignumber.js'
+import { LoanMasterNodeRegTestContainer } from '@src/module.api/loan_container'
+import { NotFoundException } from '@nestjs/common'
+import { Testing } from '@defichain/jellyfish-testing'
+import { LoanCollateralController } from '@src/module.api/loan.collateral.controller'
+
+const container = new LoanMasterNodeRegTestContainer()
+let app: NestFastifyApplication
+let controller: LoanCollateralController
+
+let collateralTokenId1: string
+let collateralTokenId2: string
+let collateralTokenId3: string
+let collateralTokenId4: string
+
+beforeAll(async () => {
+  await container.start()
+  await container.waitForWalletCoinbaseMaturity()
+  await container.waitForWalletBalanceGTE(100)
+
+  app = await createTestingApp(container)
+  const testing = Testing.create(container)
+  controller = app.get(LoanCollateralController)
+
+  await testing.token.create({ symbol: 'AAPL' })
+  await testing.generate(1)
+
+  await testing.token.create({ symbol: 'TSLA' })
+  await testing.generate(1)
+
+  await testing.token.create({ symbol: 'MSFT' })
+  await testing.generate(1)
+
+  await testing.token.create({ symbol: 'FB' })
+  await testing.generate(1)
+
+  await testing.rpc.oracle.appointOracle(await container.getNewAddress(),
+    [
+      { token: 'AAPL', currency: 'USD' },
+      { token: 'TSLA', currency: 'USD' },
+      { token: 'MSFT', currency: 'USD' },
+      { token: 'FB', currency: 'USD' }
+    ], { weightage: 1 })
+  await testing.generate(1)
+
+  collateralTokenId1 = await testing.rpc.loan.setCollateralToken({
+    token: 'AAPL',
+    factor: new BigNumber(0.1),
+    priceFeedId: 'AAPL/USD'
+  })
+  await testing.generate(1)
+
+  collateralTokenId2 = await testing.rpc.loan.setCollateralToken({
+    token: 'TSLA',
+    factor: new BigNumber(0.2),
+    priceFeedId: 'TSLA/USD'
+  })
+  await testing.generate(1)
+
+  collateralTokenId3 = await testing.rpc.loan.setCollateralToken({
+    token: 'MSFT',
+    factor: new BigNumber(0.3),
+    priceFeedId: 'MSFT/USD'
+  })
+  await testing.generate(1)
+
+  collateralTokenId4 = await testing.rpc.loan.setCollateralToken({
+    token: 'FB',
+    factor: new BigNumber(0.4),
+    priceFeedId: 'FB/USD'
+  })
+  await testing.generate(1)
+})
+
+afterAll(async () => {
+  await stopTestingApp(container, app)
+})
+
+describe('loan', () => {
+  it('should listCollateralTokens', async () => {
+    const result = await controller.list({ size: 100 })
+    expect(result.data.length).toStrictEqual(4)
+    expect(result.data).toStrictEqual([
+      {
+        id: collateralTokenId1,
+        token: 'AAPL',
+        priceFeedId: 'AAPL/USD',
+        factor: new BigNumber(0.1),
+        activateAfterBlock: new BigNumber(107)
+      },
+      {
+        id: collateralTokenId4,
+        token: 'FB',
+        priceFeedId: 'FB/USD',
+        factor: new BigNumber(0.4),
+        activateAfterBlock: new BigNumber(110)
+      },
+      {
+        id: collateralTokenId3,
+        token: 'MSFT',
+        priceFeedId: 'MSFT/USD',
+        factor: new BigNumber(0.3),
+        activateAfterBlock: new BigNumber(109)
+      },
+      {
+        id: collateralTokenId2,
+        token: 'TSLA',
+        priceFeedId: 'TSLA/USD',
+        factor: new BigNumber(0.2),
+        activateAfterBlock: new BigNumber(108)
+      }
+    ])
+  })
+
+  it('should listCollateralTokens with pagination', async () => {
+    const first = await controller.list({ size: 2 })
+
+    expect(first.data.length).toStrictEqual(2)
+    expect(first.page?.next).toStrictEqual('FB')
+
+    expect(first.data[0].token).toStrictEqual('AAPL')
+    expect(first.data[1].token).toStrictEqual('FB')
+
+    const next = await controller.list({
+      size: 2,
+      next: first.page?.next
+    })
+
+    expect(next.data.length).toStrictEqual(2)
+    expect(next.page?.next).toStrictEqual('TSLA')
+
+    expect(next.data[0].token).toStrictEqual('MSFT')
+    expect(next.data[1].token).toStrictEqual('TSLA')
+
+    const last = await controller.list({
+      size: 2,
+      next: next.page?.next
+    })
+
+    expect(last.data.length).toStrictEqual(0)
+    expect(last.page).toBeUndefined()
+  })
+
+  it('should listCollateralTokens with an empty object if size 100 next 300 which is out of range', async () => {
+    const result = await controller.list({ size: 100, next: '300' })
+
+    expect(result.data.length).toStrictEqual(0)
+    expect(result.page).toBeUndefined()
+  })
+})
+
+describe('get', () => {
+  it('should get collateral token by symbol', async () => {
+    const data = await controller.get('AAPL')
+    expect(data).toStrictEqual({
+      [collateralTokenId1]: {
+        token: 'AAPL',
+        factor: new BigNumber(0.1),
+        priceFeedId: 'AAPL/USD',
+        activateAfterBlock: new BigNumber(107)
+      }
+    })
+  })
+
+  it('should throw error while getting non-existent collateral token', async () => {
+    expect.assertions(2)
+    try {
+      await controller.get('999')
+    } catch (err) {
+      expect(err).toBeInstanceOf(NotFoundException)
+      expect(err.response).toStrictEqual({
+        statusCode: 404,
+        message: 'Unable to find collateral token',
+        error: 'Not Found'
+      })
+    }
+  })
+})
