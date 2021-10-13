2cd213c46a7c8c4ee69423135cef47652f73056b
Ivan <ivan@zynesis.com>
2021-10-13 11:42:48 +0800

    remove log

--- src/module.indexer/model/script.activity.v2/token.activity/dex/add.liquidity.ts
+++ src/module.indexer/model/script.activity.v2/token.activity/dex/add.liquidity.ts
@@ -7,7 +7,6 @@ export class AddLiquidityIndexer extends TokenActivityIndexer<PoolAddLiquidity> 
   OP_CODE: number = CPoolAddLiquidity.OP_CODE
 
   async extractTokenActivities (addLiquidity: PoolAddLiquidity): Promise<ScriptTokenActivity[]> {
-    console.log('extractTokenActivities', 'add liq')
     const result: ScriptTokenActivity[] = []
 
     for (const { script, balances } of addLiquidity.from) {
