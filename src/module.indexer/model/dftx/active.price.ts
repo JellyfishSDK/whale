1f3c9c2986b70a80a2329c1f6bfcb1a86b8d80c4
Ahmed Hilali <monstrobishi@gmail.com>
2021-11-17 22:18:49 +0800

    New active price implementation

--- src/module.indexer/model/dftx/active.price.ts
+++ src/module.indexer/model/dftx/active.price.ts
@@ -1,15 +1,18 @@
 import { NetworkName } from '@defichain/jellyfish-network'
+import { CSetLoanToken, SetLoanToken } from '@defichain/jellyfish-transaction'
 import { Inject, Injectable } from '@nestjs/common'
-import { Indexer, RawBlock } from '@src/module.indexer/model/_abstract'
+import { RawBlock } from '@src/module.indexer/model/_abstract'
 import { OraclePriceActive, OraclePriceActiveMapper } from '@src/module.model/oracle.price.active'
 import { OraclePriceAggregated, OraclePriceAggregatedMapper } from '@src/module.model/oracle.price.aggregated'
-import { PriceTickerMapper } from '@src/module.model/price.ticker'
+import { PriceTickerMapper, PriceTicker } from '@src/module.model/price.ticker'
 import { HexEncoder } from '@src/module.model/_hex.encoder'
-import { PriceTicker } from '@whale-api-client/api/prices'
 import BigNumber from 'bignumber.js'
+import { DfTxIndexer, DfTxTransaction } from './_abstract'
 
 @Injectable()
-export class ActivePriceIndexer extends Indexer {
+export class ActivePriceIndexer extends DfTxIndexer<SetLoanToken> {
+  OP_CODE: number = CSetLoanToken.OP_CODE
+
   BLOCK_INTERVAL: number
   DEVIATION_THRESHOLD: number = 0.3
   MINIMUM_LIVE_ORACLES: number = 2
@@ -24,26 +27,39 @@ export class ActivePriceIndexer extends Indexer {
     this.BLOCK_INTERVAL = network === 'regtest' ? 6 : 120
   }
 
-  async index (block: RawBlock): Promise<void> {
-    if (block.height % this.BLOCK_INTERVAL !== 0) {
+  async index (block: RawBlock, txns: Array<DfTxTransaction<SetLoanToken>>): Promise<void> {
+    if (block.height % this.BLOCK_INTERVAL === 0) {
+      await this.performActivePriceTickForAll(block)
+      // return early since we updated all the price ticks already
       return
     }
 
+    // only update for specific setLoanToken tx (to match ain implementation)
+    for (const { dftx: { data } } of txns) {
+      await this.performActivePriceTick(block, `${data.currencyPair.token}-${data.currencyPair.currency}`)
+    }
+  }
+
+  async performActivePriceTickForAll (block: RawBlock): Promise<void> {
     const tickers: PriceTicker[] = await this.priceTickerMapper.query(Number.MAX_SAFE_INTEGER)
     for (const ticker of tickers) {
-      const aggregatedPrices = await this.aggregatedMapper.query(ticker.id, 1)
-      if (aggregatedPrices.length < 1) {
-        continue
-      }
-
-      const previousPrices = await this.activePriceMapper.query(ticker.id, 1)
-      await this.activePriceMapper.put(this.mapActivePrice(block, ticker, aggregatedPrices[0], previousPrices[0]))
+      await this.performActivePriceTick(block, ticker.id)
     }
+  }
+
+  async performActivePriceTick (block: RawBlock, tickerId: string): Promise<void> {
+    const aggregatedPrices = await this.aggregatedMapper.query(tickerId, 1)
+    if (aggregatedPrices.length < 1) {
+      return
+    }
+
+    const previousPrices = await this.activePriceMapper.query(tickerId, 1)
+    await this.activePriceMapper.put(this.mapActivePrice(block, tickerId, aggregatedPrices[0], previousPrices[0]))
   }
 
   private mapActivePrice (
     block: RawBlock,
-    ticker: PriceTicker,
+    tickerId: string,
     aggregatedPrice: OraclePriceAggregated,
     previousActive?: OraclePriceActive
   ): OraclePriceActive {
@@ -51,8 +67,8 @@ export class ActivePriceIndexer extends Indexer {
     const activePrice = previousActive?.next !== undefined ? previousActive.next : previousActive?.active
 
     return {
-      id: `${ticker.id}-${block.height}`,
-      key: ticker.id,
+      id: `${tickerId}-${block.height}`,
+      key: tickerId,
       isLive: this.isLive(activePrice, nextPrice),
       block: { hash: block.hash, height: block.height, medianTime: block.mediantime, time: block.time },
       active: activePrice,
@@ -106,7 +122,7 @@ export class ActivePriceIndexer extends Indexer {
     return true
   }
 
-  async invalidate (block: RawBlock): Promise<void> {
+  async invalidate (block: RawBlock, _: Array<DfTxTransaction<SetLoanToken>>): Promise<void> {
     if (block.height % this.BLOCK_INTERVAL !== 0) {
       return
     }
