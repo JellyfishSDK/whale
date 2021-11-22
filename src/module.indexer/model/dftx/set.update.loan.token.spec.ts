b0bbdadb3b5a5f17de42351a4152e5d8fa883f2f
ivan-zynesis <ivan@zynesis.com>
2021-11-22 17:18:25 +0800

    added test

--- src/module.indexer/model/dftx/set.update.loan.token.spec.ts
+++ src/module.indexer/model/dftx/set.update.loan.token.spec.ts
@@ -5,6 +5,7 @@ import { JsonRpcClient } from '@defichain/jellyfish-api-jsonrpc'
 import BigNumber from 'bignumber.js'
 import { LoanTokenMapper } from '@src/module.model/loan.token'
 import { LoanTokenHistoryMapper } from '@src/module.model/loan.token.history'
+import { TokenMapper } from '@src/module.model/token'
 
 const container = new MasterNodeRegTestContainer()
 let app: NestFastifyApplication
@@ -42,6 +43,7 @@ beforeAll(async () => {
     mintable: false,
     interest: new BigNumber(0.03)
   })
+  await container.generate(1)
   const amznLoanTokenId = await client.loan.setLoanToken({
     symbol: 'AMZN',
     name: 'armazone',
@@ -57,7 +59,7 @@ beforeAll(async () => {
     name: 'amazing',
     fixedIntervalPriceId: 'AMZG/EUR',
     mintable: false,
-    interest: new BigNumber(0.06) // intentional keep the same
+    interest: new BigNumber(0.08) // intentional keep the same
   })
   await container.generate(2)
 
@@ -69,20 +71,121 @@ afterAll(async () => {
   await stopTestingApp(container, app)
 })
 
-describe('created loan token', () => {
-  it('should index tokens', async () => {
-    console.log('loan tokens: ')
-    console.log(await app.get(LoanTokenMapper).query(100))
-    console.log('========================================')
+describe('token', () => {
+  it('should create/update normal token', async () => {
+    const tokens = await app.get(TokenMapper).query(100)
+    const aapl = tokens.find(t => t.symbol === 'AAPL')
 
-    console.log('loan tokens history page 1: ')
+    expect(aapl).toBeDefined()
+    expect(aapl?.symbol).toStrictEqual('AAPL')
+    expect(aapl?.name).toStrictEqual('appol')
+    expect(aapl?.mintable).toStrictEqual(false)
+
+    const amzn = tokens.find(t => t.symbol === 'AMZG')
+    expect(amzn).toBeDefined()
+    expect(amzn?.symbol).toStrictEqual('AMZG')
+    expect(amzn?.name).toStrictEqual('amazing')
+    expect(amzn?.mintable).toStrictEqual(false) // updated by updateLoanToken
+  })
+})
+
+describe('loan token', () => {
+  it('each loan token should be unique', async () => {
+    const tokens = await app.get(TokenMapper).query(100)
+    const aaplT = tokens.find(t => t.symbol === 'AAPL')
+    const amznT = tokens.find(t => t.symbol === 'AMZG')
+
+    const loanTokens = await app.get(LoanTokenMapper).query(100)
+    expect(loanTokens.length).toStrictEqual(2)
+    const amzn = loanTokens.find(lt => lt.tokenId === amznT?.id)
+    const aapl = loanTokens.find(lt => lt.tokenId === aaplT?.id)
+
+    expect(amzn).toBeDefined()
+    expect(amzn?.interest).toStrictEqual('0.08') // latest value
+    expect(amzn?.tokenCurrency).toStrictEqual('AMZG-EUR') // latest value
+    expect(amzn?.tokenId).toStrictEqual('2')
+    expect(amzn?.block).toBeDefined()
+    expect(amzn?.block.height).toStrictEqual(110)
+
+    expect(aapl).toBeDefined()
+    expect(aapl?.interest).toStrictEqual('0.03')
+    expect(aapl?.tokenCurrency).toStrictEqual('AAPL-EUR')
+    expect(aapl?.tokenId).toStrictEqual('1')
+    expect(aapl?.block).toBeDefined()
+    expect(aapl?.block.height).toStrictEqual(109)
+  })
+})
+
+describe('loan token history', () => {
+  it('should index without partition (query without filter)', async () => {
+    const allHistory = await app.get(LoanTokenHistoryMapper).queryAll(100)
+    expect(allHistory.length).toStrictEqual(3)
+
+    const [one, two, three] = allHistory
+
     const page1 = await app.get(LoanTokenHistoryMapper).queryAll(2)
-    console.log(page1)
-    console.log('========================================')
+    expect(page1.length).toStrictEqual(2)
+    expect(page1[0]).toStrictEqual(one)
+    expect(one.symbol).toStrictEqual('AMZG')
+    expect(one.name).toStrictEqual('amazing')
+    expect(one.interest).toStrictEqual('0.08')
+    expect(one.tokenCurrency).toStrictEqual('AMZG-EUR')
+    expect(one.block).toBeDefined()
+    expect(one.block.height).toStrictEqual(113)
 
-    console.log('loan tokens history page 2: ')
-    const page2 = await app.get(LoanTokenHistoryMapper).queryAll(2, page1[1].loanTokenId)
-    console.log(page2)
-    console.log('========================================')
+    expect(page1[1]).toStrictEqual(two)
+    expect(two.symbol).toStrictEqual('AMZN')
+    expect(two.name).toStrictEqual('armazone')
+    expect(two.interest).toStrictEqual('0.06')
+    expect(two.tokenCurrency).toStrictEqual('AMZN-USD')
+    expect(two.block).toBeDefined()
+    expect(two.block.height).toStrictEqual(110)
+
+    const page2 = await app.get(LoanTokenHistoryMapper).queryAll(2, page1[1].id)
+    expect(page2.length).toStrictEqual(1)
+    expect(page2[0]).toStrictEqual(three)
+    expect(three.symbol).toStrictEqual('AAPL')
+    expect(three.name).toStrictEqual('appol')
+    expect(three.interest).toStrictEqual('0.03')
+    expect(three.tokenCurrency).toStrictEqual('AAPL-EUR')
+    expect(three.block).toBeDefined()
+    expect(three.block.height).toStrictEqual(109)
+  })
+
+  it('should index and partitioned by loanTokenId', async () => {
+    const tokens = await app.get(TokenMapper).query(100)
+    const aaplT = tokens.find(t => t.symbol === 'AAPL')
+    const amznT = tokens.find(t => t.symbol === 'AMZG')
+
+    const loanTokens = await app.get(LoanTokenMapper).query(100)
+    expect(loanTokens.length).toStrictEqual(2)
+    const aapl = loanTokens.find(lt => lt.tokenId === aaplT?.id)
+    expect(aapl).toBeDefined()
+    const amzn = loanTokens.find(lt => lt.tokenId === amznT?.id)
+    expect(amzn).toBeDefined()
+
+    const aaplHistory = await app.get(LoanTokenHistoryMapper).query(aapl?.id as string, 100)
+    expect(aaplHistory.length).toStrictEqual(1)
+    expect(aaplHistory[0].symbol).toStrictEqual('AAPL')
+    expect(aaplHistory[0].name).toStrictEqual('appol')
+    expect(aaplHistory[0].interest).toStrictEqual('0.03')
+    expect(aaplHistory[0].tokenCurrency).toStrictEqual('AAPL-EUR')
+    expect(aaplHistory[0].block).toBeDefined()
+    expect(aaplHistory[0].block.height).toStrictEqual(109)
+
+    const amznHistory = await app.get(LoanTokenHistoryMapper).query(amzn?.id as string, 100)
+    expect(amznHistory.length).toStrictEqual(2)
+    expect(amznHistory[0].symbol).toStrictEqual('AMZG')
+    expect(amznHistory[0].name).toStrictEqual('amazing')
+    expect(amznHistory[0].interest).toStrictEqual('0.08')
+    expect(amznHistory[0].tokenCurrency).toStrictEqual('AMZG-EUR')
+    expect(amznHistory[0].block).toBeDefined()
+    expect(amznHistory[0].block.height).toStrictEqual(113)
+    expect(amznHistory[1].symbol).toStrictEqual('AMZN')
+    expect(amznHistory[1].name).toStrictEqual('armazone')
+    expect(amznHistory[1].interest).toStrictEqual('0.06')
+    expect(amznHistory[1].tokenCurrency).toStrictEqual('AMZN-USD')
+    expect(amznHistory[1].block).toBeDefined()
+    expect(amznHistory[1].block.height).toStrictEqual(110)
   })
 })
--- src/module.indexer/model/dftx/update.loan.token.ts
+++ src/module.indexer/model/dftx/update.loan.token.ts
@@ -18,78 +18,76 @@ export class UpdateLoanTokenIndexer extends DfTxIndexer<UpdateLoanToken> {
     super()
   }
 
-  async index (block: RawBlock, txns: Array<DfTxTransaction<UpdateLoanToken>>): Promise<void> {
-    for (const { dftx: { data } } of txns) {
-      const [existingLoanToken, existingToken] = await this._findExisting(data.tokenTx)
+  async indexTransaction (block: RawBlock, txn: DfTxTransaction<UpdateLoanToken>): Promise<void> {
+    const { data } = txn.dftx
+    const [existingLoanToken, existingToken] = await this._findExisting(data.tokenTx)
 
-      const symbol = data.symbol.trim().substr(0, MAX_TOKEN_SYMBOL_LENGTH)
-      const name = data?.name?.trim().substr(0, MAX_TOKEN_NAME_LENGTH) ?? data.symbol.trim().substr(0, MAX_TOKEN_NAME_LENGTH)
-      const interest = data.interest.toFixed()
-      const tokenCurrency = `${data.currencyPair.token}-${data.currencyPair.currency}`
-      const blockObj = {
-        hash: block.hash,
-        height: block.height,
-        time: block.time,
-        medianTime: block.mediantime
-      }
-
-      // update token
-      await this.tokenMapper.put({
-        ...existingToken,
-        symbol: symbol,
-        name: name,
-        mintable: data.mintable
-        // block: blockObj // TBD: this info can be referenced as token creation date
-      })
-
-      // update loan token
-      await this.loanTokenMapper.put({
-        ...existingLoanToken,
-        interest: interest,
-        tokenCurrency: tokenCurrency
-        // block: blockObj // TBD: this info can be referenced as token creation date
-      })
-
-      // write new history
-      await this.loanTokenHistoryMapper.put({
-        id: `${block.height}-${data.tokenTx}`,
-        loanTokenId: `${data.tokenTx}`,
-        symbol: symbol,
-        name: name,
-        interest: interest,
-        mintable: data.mintable,
-        tokenCurrency: tokenCurrency,
-        block: blockObj
-      })
+    const symbol = data.symbol.trim().substr(0, MAX_TOKEN_SYMBOL_LENGTH)
+    const name = data?.name?.trim().substr(0, MAX_TOKEN_NAME_LENGTH) ?? data.symbol.trim().substr(0, MAX_TOKEN_NAME_LENGTH)
+    const interest = data.interest.toFixed()
+    const tokenCurrency = `${data.currencyPair.token}-${data.currencyPair.currency}`
+    const blockObj = {
+      hash: block.hash,
+      height: block.height,
+      time: block.time,
+      medianTime: block.mediantime
     }
+
+    // update token
+    await this.tokenMapper.put({
+      ...existingToken,
+      symbol: symbol,
+      name: name,
+      mintable: data.mintable
+      // block: blockObj // TBD: this info can be referenced as token creation date
+    })
+
+    // update loan token
+    await this.loanTokenMapper.put({
+      ...existingLoanToken,
+      interest: interest,
+      tokenCurrency: tokenCurrency
+      // block: blockObj // TBD: this info can be referenced as token creation date
+    })
+
+    // write new history
+    await this.loanTokenHistoryMapper.put({
+      id: `${block.height}-${data.tokenTx}`,
+      loanTokenId: `${data.tokenTx}`,
+      symbol: symbol,
+      name: name,
+      interest: interest,
+      mintable: data.mintable,
+      tokenCurrency: tokenCurrency,
+      block: blockObj
+    })
   }
 
-  async invalidate (block: RawBlock, txns: Array<DfTxTransaction<UpdateLoanToken>>): Promise<void> {
-    for (const { dftx: { data } } of txns) {
-      const [existingLoanToken, existingToken] = await this._findExisting(data.tokenTx)
+  async invalidateTransaction (block: RawBlock, txn: DfTxTransaction<UpdateLoanToken>): Promise<void> {
+    const { data } = txn.dftx
+    const [existingLoanToken, existingToken] = await this._findExisting(data.tokenTx)
 
-      const [lastVersion] = await this.loanTokenHistoryMapper.query(data.tokenTx, 1, `${block.height}`)
+    const [lastVersion] = await this.loanTokenHistoryMapper.query(data.tokenTx, 1, `${block.height}`)
 
-      // restore token
-      await this.tokenMapper.put({
-        ...existingToken,
-        symbol: lastVersion.symbol,
-        name: lastVersion.name,
-        mintable: lastVersion.mintable
-        // block: createOrUpdate.block // TBD: this info can be referenced as token creation date
-      })
+    // restore token
+    await this.tokenMapper.put({
+      ...existingToken,
+      symbol: lastVersion.symbol,
+      name: lastVersion.name,
+      mintable: lastVersion.mintable
+      // block: createOrUpdate.block // TBD: this info can be referenced as token creation date
+    })
 
-      // restore loan token
-      await this.loanTokenMapper.put({
-        ...existingLoanToken,
-        interest: lastVersion.interest,
-        tokenCurrency: lastVersion.tokenCurrency
-        // block: createOrUpdate.block // TBD: this info can be referenced as token creation date
-      })
+    // restore loan token
+    await this.loanTokenMapper.put({
+      ...existingLoanToken,
+      interest: lastVersion.interest,
+      tokenCurrency: lastVersion.tokenCurrency
+      // block: createOrUpdate.block // TBD: this info can be referenced as token creation date
+    })
 
-      // delete update history
-      await this.loanTokenHistoryMapper.delete(`${block.height}-${data.tokenTx}`)
-    }
+    // delete update history
+    await this.loanTokenHistoryMapper.delete(`${block.height}-${data.tokenTx}`)
   }
 
   private async _findExisting (loanTokenId: string): Promise<[LoanToken, Token]> {
