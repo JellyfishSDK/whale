062e4d0c78e92ff37cf99203035a0cb5dd9ca154
Ahmed Hilali <monstrobishi@gmail.com>
2021-11-22 12:10:59 +0800

    Adapted to new changes

--- src/module.indexer/model/dftx/compositeswap.ts
+++ src/module.indexer/model/dftx/compositeswap.ts
@@ -7,6 +7,7 @@ import { PoolPairMapper } from '@src/module.model/poolpair'
 import { PoolPairTokenMapper } from '@src/module.model/poolpair.token'
 import { NetworkName } from '@defichain/jellyfish-network'
 import BigNumber from 'bignumber.js'
+import { IndexerError } from '@src/module.indexer/error'
 
 @Injectable()
 export class CompositeSwapIndexer extends DfTxIndexer<CompositeSwap> {
@@ -20,65 +21,65 @@ export class CompositeSwapIndexer extends DfTxIndexer<CompositeSwap> {
     super()
   }
 
-  async index (block: RawBlock, txns: Array<DfTxTransaction<CompositeSwap>>): Promise<void> {
-    for (const { dftx: { data } } of txns) {
-      const poolSwap = data.poolSwap
-      const poolIds = data.pools
-      if (poolIds.length === 0) {
-        const poolPairToken = await this.poolPairTokenMapper.queryForTokenPair(poolSwap.fromTokenId, poolSwap.toTokenId)
+  async indexTransaction (block: RawBlock, transaction: DfTxTransaction<CompositeSwap>): Promise<void> {
+    const data = transaction.dftx.data
+    const poolSwap = data.poolSwap
+    const poolIds = data.pools
+    if (poolIds.length === 0) {
+      const poolPairToken = await this.poolPairTokenMapper.queryForTokenPair(poolSwap.fromTokenId, poolSwap.toTokenId)
 
-        if (poolPairToken === undefined) {
-          continue
-        }
-
-        poolIds.push({ id: poolPairToken.poolPairId })
+      if (poolPairToken === undefined) {
+        throw new IndexerError(`Pool for pair ${poolSwap.fromTokenId}, ${poolSwap.toTokenId} not found`)
       }
 
-      let previousAmount = { fromTokenId: -1, fromAmount: new BigNumber(0) }
-      for (const pool of poolIds) {
-        const poolPair = await this.poolPairMapper.getLatest(`${pool.id}`)
+      poolIds.push({ id: poolPairToken.poolPairId })
+    }
 
-        let fromTokenId = poolSwap.fromTokenId
-        let fromAmount = poolSwap.fromAmount
+    let previousAmount = { fromTokenId: -1, fromAmount: new BigNumber(0) }
+    for (const pool of poolIds) {
+      const poolPair = await this.poolPairMapper.getLatest(`${pool.id}`)
 
-        if (previousAmount.fromTokenId !== -1) {
-          fromTokenId = previousAmount.fromTokenId
-          fromAmount = previousAmount.fromAmount
-        }
+      let fromTokenId = poolSwap.fromTokenId
+      let fromAmount = poolSwap.fromAmount
 
-        if (poolPair !== undefined) {
-          const BayFrontGardensHeight = PoolswapConsensusParams[this.network].BayFrontGardensHeight
-          const swappedPoolResults = PoolSwapIndexer.executeSwap(poolPair, fromTokenId, fromAmount,
-            block.height > BayFrontGardensHeight)
-
-          const swappedPoolpair = swappedPoolResults.poolPair
-          swappedPoolpair.id = `${poolPair.poolPairId}-${block.height}`
-          swappedPoolpair.block = { hash: block.hash, height: block.height, medianTime: block.mediantime, time: block.time }
-          await this.poolPairMapper.put(swappedPoolpair)
-
-          previousAmount = { fromAmount: swappedPoolResults.result.swapped, fromTokenId: swappedPoolResults.tokenOut }
-        }
+      if (previousAmount.fromTokenId !== -1) {
+        fromTokenId = previousAmount.fromTokenId
+        fromAmount = previousAmount.fromAmount
       }
+
+      if (poolPair === undefined) {
+        throw new IndexerError(`Pool with id ${pool.id} not found`)
+      }
+
+      const BayFrontGardensHeight = PoolswapConsensusParams[this.network].BayFrontGardensHeight
+      const swappedPoolResults = PoolSwapIndexer.executeSwap(poolPair, fromTokenId, fromAmount,
+        block.height > BayFrontGardensHeight)
+
+      const swappedPoolpair = swappedPoolResults.poolPair
+      swappedPoolpair.id = `${poolPair.poolPairId}-${block.height}`
+      swappedPoolpair.block = { hash: block.hash, height: block.height, medianTime: block.mediantime, time: block.time }
+      await this.poolPairMapper.put(swappedPoolpair)
+
+      previousAmount = { fromAmount: swappedPoolResults.result.swapped, fromTokenId: swappedPoolResults.tokenOut }
     }
   }
 
-  async invalidate (block: RawBlock, txns: Array<DfTxTransaction<CompositeSwap>>): Promise<void> {
-    for (const { dftx: { data } } of txns) {
-      const poolSwap = data.poolSwap
-      const poolIds = data.pools
-      if (poolIds.length === 0) {
-        const poolPairToken = await this.poolPairTokenMapper.queryForTokenPair(poolSwap.fromTokenId, poolSwap.toTokenId)
+  async invalidateTransaction (block: RawBlock, transaction: DfTxTransaction<CompositeSwap>): Promise<void> {
+    const data = transaction.dftx.data
+    const poolSwap = data.poolSwap
+    const poolIds = data.pools
+    if (poolIds.length === 0) {
+      const poolPairToken = await this.poolPairTokenMapper.queryForTokenPair(poolSwap.fromTokenId, poolSwap.toTokenId)
 
-        if (poolPairToken === undefined) {
-          continue
-        }
-
-        poolIds.push({ id: poolPairToken.poolPairId })
+      if (poolPairToken === undefined) {
+        throw new IndexerError(`Pool for pair ${poolSwap.fromTokenId}, ${poolSwap.toTokenId} not found`)
       }
 
-      for (const pool of poolIds) {
-        await this.poolPairMapper.delete(`${pool.id}-${block.height}`)
-      }
+      poolIds.push({ id: poolPairToken.poolPairId })
+    }
+
+    for (const pool of poolIds) {
+      await this.poolPairMapper.delete(`${pool.id}-${block.height}`)
     }
   }
 }
