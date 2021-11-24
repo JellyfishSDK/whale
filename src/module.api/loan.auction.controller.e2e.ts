cefb6b2deb7cf6750278110f2746b8096693ee6e
jingyi2811 <jingyi2811@gmail.com>
2021-11-24 19:17:20 +0800

    Minor

--- src/module.api/loan.auction.controller.e2e.ts
+++ src/module.api/loan.auction.controller.e2e.ts
@@ -341,6 +341,7 @@ describe('list', () => {
 
   it('should listAuctions with an empty object if size 100 next 51f6233c4403f6ce113bb4e90f83b176587f401081605b8a8bb723ff3b0ab5b6 300 which is out of range', async () => {
     const result = await controller.listAuction({ size: 100, next: '51f6233c4403f6ce113bb4e90f83b176587f401081605b8a8bb723ff3b0ab5b6300' })
+
     expect(result.data.length).toStrictEqual(0)
     expect(result.page).toBeUndefined()
   })
