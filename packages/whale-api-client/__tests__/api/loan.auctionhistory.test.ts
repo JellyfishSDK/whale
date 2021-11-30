b21cff6dd6baf932aec401dba96aac3ae8076170
websterlcl <w.canonbrother@gmail.com>
2021-11-30 13:51:00 +0800

    refactor test.ts

--- packages/whale-api-client/__tests__/api/loan.auctionhistory.test.ts
+++ packages/whale-api-client/__tests__/api/loan.auctionhistory.test.ts
@@ -1,7 +1,7 @@
 import { StubWhaleApiClient } from '../stub.client'
 import { StubService } from '../stub.service'
 import BigNumber from 'bignumber.js'
-import { TestingGroup } from '@defichain/jellyfish-testing'
+import { TestingGroup, Testing } from '@defichain/jellyfish-testing'
 import { GenesisKeys, MasterNodeRegTestContainer } from '@defichain/testcontainers'
 
 const tGroup = TestingGroup.create(2, i => new MasterNodeRegTestContainer(GenesisKeys[i]))
@@ -11,350 +11,127 @@ const bob = tGroup.get(1)
 const service = new StubService(alice.container)
 const client = new StubWhaleApiClient(service)
 
-let aliceColAddr: string
-let bobColAddr: string
-let vaultId1: string // Alice 1st vault
-let vaultId2: string // Alice 2nd vault
-let vaultId3: string // Bob 1st vault
-let vaultId4: string // Bob 2nd vault
-
 beforeAll(async () => {
   await tGroup.start()
   await alice.container.waitForWalletCoinbaseMaturity()
-  await alice.container.waitForWalletBalanceGTE(100)
   await service.start()
+  const aliceColAddr = await alice.generateAddress()
+  const bobColAddr = await bob.generateAddress()
 
-  aliceColAddr = await alice.generateAddress()
-  bobColAddr = await bob.generateAddress()
-
-  await alice.token.dfi({
-    address: aliceColAddr,
-    amount: 35000
-  })
-  await alice.generate(1)
-
-  await alice.token.create({
-    symbol: 'BTC',
-    collateralAddress: aliceColAddr
-  })
-  await alice.generate(1)
-
-  await alice.token.mint({
-    symbol: 'BTC',
-    amount: 50
-  })
-  await alice.generate(1)
-
+  await dfi(alice, aliceColAddr, 35000)
+  await createToken(alice, 'BTC', aliceColAddr)
+  await mintTokens(alice, 'BTC', 50)
   await alice.rpc.account.sendTokensToAddress({}, { [bobColAddr]: ['25@BTC'] })
-  await alice.generate(1)
-  await tGroup.waitForSync()
-
-  await bob.token.dfi({
-    address: bobColAddr,
-    amount: 75000
-  })
-  await bob.generate(1)
-  await tGroup.waitForSync()
-
-  // Loan scheme
   await alice.container.call('createloanscheme', [100, 1, 'default'])
   await alice.generate(1)
 
-  // Price oracle
-  const addr = await alice.generateAddress()
   const priceFeeds = [
-    {
-      token: 'DFI',
-      currency: 'USD'
-    },
-    {
-      token: 'BTC',
-      currency: 'USD'
-    },
-    {
-      token: 'AAPL',
-      currency: 'USD'
-    },
-    {
-      token: 'TSLA',
-      currency: 'USD'
-    },
-    {
-      token: 'MSFT',
-      currency: 'USD'
-    },
-    {
-      token: 'FB',
-      currency: 'USD'
-    }
+    { token: 'DFI', currency: 'USD' },
+    { token: 'BTC', currency: 'USD' },
+    { token: 'AAPL', currency: 'USD' },
+    { token: 'TSLA', currency: 'USD' },
+    { token: 'MSFT', currency: 'USD' },
+    { token: 'FB', currency: 'USD' }
   ]
-  const oracleId = await alice.rpc.oracle.appointOracle(addr, priceFeeds, { weightage: 1 })
+  const oracleId = await alice.rpc.oracle.appointOracle(await alice.generateAddress(), priceFeeds, { weightage: 1 })
   await alice.generate(1)
-  const timestamp = Math.floor(new Date().getTime() / 1000)
-  await alice.rpc.oracle.setOracleData(oracleId, timestamp, {
-    prices: [{
-      tokenAmount: '1@DFI',
-      currency: 'USD'
-    }]
-  })
-  await alice.generate(1)
-  await alice.rpc.oracle.setOracleData(oracleId, timestamp, {
-    prices: [{
-      tokenAmount: '10000@BTC',
-      currency: 'USD'
-    }]
-  })
-  await alice.generate(1)
-  await alice.rpc.oracle.setOracleData(oracleId, timestamp, {
-    prices: [{
-      tokenAmount: '2@AAPL',
-      currency: 'USD'
-    }]
-  })
-  await alice.generate(1)
-  await alice.rpc.oracle.setOracleData(oracleId, timestamp, {
-    prices: [{
-      tokenAmount: '2@TSLA',
-      currency: 'USD'
-    }]
-  })
-  await alice.generate(1)
-  await alice.rpc.oracle.setOracleData(oracleId, timestamp, {
-    prices: [{
-      tokenAmount: '2@MSFT',
-      currency: 'USD'
-    }]
-  })
-  await alice.generate(1)
-  await alice.rpc.oracle.setOracleData(oracleId, timestamp, {
-    prices: [{
-      tokenAmount: '2@FB',
-      currency: 'USD'
-    }]
+  await alice.rpc.oracle.setOracleData(oracleId, now(), {
+    prices: [
+      { tokenAmount: '1@DFI', currency: 'USD' },
+      { tokenAmount: '10000@BTC', currency: 'USD' },
+      { tokenAmount: '2@AAPL', currency: 'USD' },
+      { tokenAmount: '2@TSLA', currency: 'USD' },
+      { tokenAmount: '2@MSFT', currency: 'USD' },
+      { tokenAmount: '2@FB', currency: 'USD' }
+    ]
   })
   await alice.generate(1)
 
-  // Collateral tokens
-  await alice.rpc.loan.setCollateralToken({
-    token: 'DFI',
-    factor: new BigNumber(1),
-    fixedIntervalPriceId: 'DFI/USD'
-  })
-  await alice.generate(1)
-  await alice.rpc.loan.setCollateralToken({
-    token: 'BTC',
-    factor: new BigNumber(1),
-    fixedIntervalPriceId: 'BTC/USD'
-  })
-  await alice.generate(1)
+  await setCollateralToken(alice, 'DFI')
+  await setCollateralToken(alice, 'BTC')
 
-  // Loan token
-  await alice.rpc.loan.setLoanToken({
-    symbol: 'AAPL',
-    fixedIntervalPriceId: 'AAPL/USD'
-  })
-  await alice.generate(1)
-  await alice.token.mint({
-    symbol: 'AAPL',
-    amount: 50000
-  })
-  await alice.generate(1)
+  await setLoanToken(alice, 'AAPL')
+  await setLoanToken(alice, 'TSLA')
+  await setLoanToken(alice, 'MSFT')
+  await setLoanToken(alice, 'FB')
 
-  await alice.rpc.loan.setLoanToken({
-    symbol: 'TSLA',
-    fixedIntervalPriceId: 'TSLA/USD'
-  })
-  await alice.generate(1)
-  await alice.token.mint({
-    symbol: 'TSLA',
-    amount: 50000
-  })
-  await alice.generate(1)
+  await mintTokens(alice, 'AAPL', 50000)
+  await mintTokens(alice, 'TSLA', 50000)
+  await mintTokens(alice, 'MSFT', 50000)
+  await mintTokens(alice, 'FB', 50000)
 
-  await alice.rpc.loan.setLoanToken({
-    symbol: 'MSFT',
-    fixedIntervalPriceId: 'MSFT/USD'
-  })
-  await alice.generate(1)
-  await alice.token.mint({
-    symbol: 'MSFT',
-    amount: 50000
-  })
-  await alice.generate(1)
+  const vaultId1 = await createVault(alice, 'default')
+  await depositToVault(alice, vaultId1, aliceColAddr, '10000@DFI')
+  await depositToVault(alice, vaultId1, aliceColAddr, '0.5@BTC')
+  await takeLoan(alice, vaultId1, '7500@AAPL')
 
-  await alice.rpc.loan.setLoanToken({
-    symbol: 'FB',
-    fixedIntervalPriceId: 'FB/USD'
-  })
-  await alice.generate(1)
-  await alice.token.mint({
-    symbol: 'FB',
-    amount: 50000
-  })
-  await alice.generate(1)
+  const vaultId2 = await createVault(alice, 'default')
+  await depositToVault(alice, vaultId2, aliceColAddr, '20000@DFI')
+  await depositToVault(alice, vaultId2, aliceColAddr, '1@BTC')
+  await takeLoan(alice, vaultId2, '15000@TSLA')
+  await tGroup.waitForSync()
 
-  // Vault 1
-  vaultId1 = await alice.rpc.container.call('createvault', [await alice.generateAddress(), 'default'])
-  await alice.generate(1)
+  await dfi(bob, bobColAddr, 75000)
 
-  await alice.container.call('deposittovault', [vaultId1, aliceColAddr, '10000@DFI'])
-  await alice.generate(1)
-  await alice.container.call('deposittovault', [vaultId1, aliceColAddr, '0.5@BTC'])
-  await alice.generate(1)
+  const vaultId3 = await createVault(bob, 'default')
+  await depositToVault(bob, vaultId3, bobColAddr, '30000@DFI')
+  await depositToVault(bob, vaultId3, bobColAddr, '1.5@BTC')
+  await takeLoan(bob, vaultId3, '22500@MSFT')
 
-  await alice.container.call('takeloan', [{
-    vaultId: vaultId1,
-    amounts: '7500@AAPL'
-  }])
-  await alice.generate(1)
-
-  // Vault 2
-  vaultId2 = await alice.rpc.container.call('createvault', [await alice.generateAddress(), 'default'])
-  await alice.generate(1)
-
-  await alice.container.call('deposittovault', [vaultId2, aliceColAddr, '20000@0DFI'])
-  await alice.generate(1)
-  await alice.container.call('deposittovault', [vaultId2, aliceColAddr, '1@BTC'])
-  await alice.generate(1)
-
-  await alice.container.call('takeloan', [{
-    vaultId: vaultId2,
-    amounts: '15000@TSLA'
-  }])
-  await alice.generate(1)
-
-  // Vault 3
-  vaultId3 = await bob.rpc.container.call('createvault', [await bob.generateAddress(), 'default'])
-  await bob.generate(1)
-
-  await bob.container.call('deposittovault', [vaultId3, bobColAddr, '30000@DFI'])
-  await bob.generate(1)
-  await bob.container.call('deposittovault', [vaultId3, bobColAddr, '1.5@BTC'])
-  await bob.generate(1)
-
-  await bob.container.call('takeloan', [{
-    vaultId: vaultId3,
-    amounts: '22500@MSFT'
-  }])
-  await bob.generate(1)
-
-  // Vault 4
-  vaultId4 = await bob.rpc.container.call('createvault', [await bob.generateAddress(), 'default'])
-  await bob.generate(1)
-
-  await bob.container.call('deposittovault', [vaultId4, bobColAddr, '40000@DFI'])
-  await bob.generate(1)
-  await bob.container.call('deposittovault', [vaultId4, bobColAddr, '2@BTC'])
-  await bob.generate(1)
-
-  await bob.container.call('takeloan', [{
-    vaultId: vaultId4,
-    amounts: '30000@FB'
-  }])
-  await bob.generate(1)
+  const vaultId4 = await createVault(bob, 'default')
+  await depositToVault(bob, vaultId4, bobColAddr, '40000@DFI')
+  await depositToVault(bob, vaultId4, bobColAddr, '2@BTC')
+  await takeLoan(bob, vaultId4, '30000@FB')
+  await tGroup.waitForSync()
 
   {
     // When there is no liquidation occurs
     const data = await alice.container.call('listauctions', [])
     expect(data).toStrictEqual([])
 
-    const vault1 = await alice.rpc.loan.getVault(vaultId1)
-    expect(vault1.state).toStrictEqual('active')
-
-    const vault2 = await alice.rpc.loan.getVault(vaultId2)
-    expect(vault2.state).toStrictEqual('active')
-
-    const vault3 = await alice.rpc.loan.getVault(vaultId3)
-    expect(vault3.state).toStrictEqual('active')
-
-    const vault4 = await alice.rpc.loan.getVault(vaultId4)
-    expect(vault4.state).toStrictEqual('active')
+    const list = await alice.container.call('listauctions')
+    expect(list.every((each: any) => each.state === 'active'))
   }
 
   // Going to liquidate the vaults by price increase of the loan tokens
-  await alice.rpc.oracle.setOracleData(oracleId, timestamp, {
-    prices: [{
-      tokenAmount: '2.2@AAPL',
-      currency: 'USD'
-    }]
+  await alice.rpc.oracle.setOracleData(oracleId, now(), {
+    prices: [{ tokenAmount: '2.2@AAPL', currency: 'USD' }]
   })
-  await alice.generate(1)
-  await alice.container.waitForActivePrice('AAPL/USD', '2.2')
-  await alice.rpc.oracle.setOracleData(oracleId, timestamp, {
-    prices: [{
-      tokenAmount: '2.2@TSLA',
-      currency: 'USD'
-    }]
+  // await alice.container.waitForActivePrice('AAPL/USD', '2.2')
+
+  await alice.rpc.oracle.setOracleData(oracleId, now(), {
+    prices: [{ tokenAmount: '2.2@TSLA', currency: 'USD' }]
   })
-  await alice.generate(1)
-  await alice.container.waitForActivePrice('TSLA/USD', '2.2')
-  await alice.rpc.oracle.setOracleData(oracleId, timestamp, {
-    prices: [{
-      tokenAmount: '2.2@MSFT',
-      currency: 'USD'
-    }]
+  // await alice.container.waitForActivePrice('TSLA/USD', '2.2')
+
+  await alice.rpc.oracle.setOracleData(oracleId, now(), {
+    prices: [{ tokenAmount: '2.2@MSFT', currency: 'USD' }]
   })
-  await alice.generate(1)
-  await alice.container.waitForActivePrice('MSFT/USD', '2.2')
-  await alice.rpc.oracle.setOracleData(oracleId, timestamp, {
-    prices: [{
-      tokenAmount: '2.2@FB',
-      currency: 'USD'
-    }]
+  // await alice.container.waitForActivePrice('MSFT/USD', '2.2')
+
+  await alice.rpc.oracle.setOracleData(oracleId, now(), {
+    prices: [{ tokenAmount: '2.2@FB', currency: 'USD' }]
   })
-  await alice.generate(1)
-  await alice.container.waitForActivePrice('FB/USD', '2.2')
-  await alice.generate(1)
+  // await alice.container.waitForActivePrice('FB/USD', '2.2')
+  await alice.container.generate(13)
 
-  // When there is liquidation
-  const list = await alice.container.call('listauctions', [])
-  list.forEach((l: { state: any }) =>
-    expect(l.state).toStrictEqual('inLiquidation')
-  )
-
-  await alice.rpc.account.sendTokensToAddress({}, { [aliceColAddr]: ['7875@AAPL'] })
-  await alice.generate(1)
-
-  await alice.rpc.account.sendTokensToAddress({}, { [aliceColAddr]: ['15750@TSLA'] })
-  await alice.generate(1)
-
-  await alice.rpc.account.sendTokensToAddress({}, { [bobColAddr]: ['23625@MSFT'] })
-  await alice.generate(1)
-
-  await alice.rpc.account.sendTokensToAddress({}, { [bobColAddr]: ['31500@FB'] })
-  await alice.generate(1)
-
-  {
-    const txid = await alice.container.call('placeauctionbid', [vaultId1, 0, aliceColAddr, '7875@AAPL'])
-    expect(typeof txid).toStrictEqual('string')
-    expect(txid.length).toStrictEqual(64)
-    await alice.generate(1)
+  { // When there is liquidation
+    const list = await alice.container.call('listauctions')
+    expect(list.every((each: any) => each.state === 'inLiquidation'))
   }
 
-  {
-    const txid = await alice.container.call('placeauctionbid', [vaultId2, 1, aliceColAddr, '15750@TSLA'])
-    expect(typeof txid).toStrictEqual('string')
-    expect(txid.length).toStrictEqual(64)
-    await alice.generate(1)
-  }
+  await alice.rpc.account.sendTokensToAddress({}, { [aliceColAddr]: ['7875@AAPL', '15750@TSLA'] })
+  await alice.rpc.account.sendTokensToAddress({}, { [bobColAddr]: ['23625@MSFT', '31500@FB'] })
+  await alice.generate(1)
 
+  await alice.container.call('placeauctionbid', [vaultId1, 0, aliceColAddr, '7875@AAPL'])
+  await alice.container.call('placeauctionbid', [vaultId2, 1, aliceColAddr, '15750@TSLA'])
+  await alice.generate(1)
   await tGroup.waitForSync()
 
-  {
-    const txid = await bob.container.call('placeauctionbid', [vaultId3, 0, bobColAddr, '23625@MSFT'])
-    expect(typeof txid).toStrictEqual('string')
-    expect(txid.length).toStrictEqual(64)
-    await bob.generate(1)
-  }
-
-  {
-    const txid = await bob.container.call('placeauctionbid', [vaultId4, 1, bobColAddr, '31500@FB'])
-    expect(typeof txid).toStrictEqual('string')
-    expect(txid.length).toStrictEqual(64)
-    await bob.generate(1)
-  }
-
-  await alice.container.generate(80)
+  await bob.container.call('placeauctionbid', [vaultId3, 0, bobColAddr, '23625@MSFT'])
+  await bob.container.call('placeauctionbid', [vaultId4, 1, bobColAddr, '31500@FB'])
+  await bob.generate(40)
   await tGroup.waitForSync()
 })
 
@@ -412,3 +189,58 @@ describe('list', () => {
     expect(last.nextToken).toBeUndefined()
   })
 })
+
+function now (): number {
+  return Math.floor(new Date().getTime() / 1000)
+}
+async function dfi (testing: Testing, address: string, amount: number): Promise<void> {
+  await testing.token.dfi({
+    address: address,
+    amount: amount
+  })
+  await testing.generate(1)
+}
+async function createToken (testing: Testing, symbol: string, address: string): Promise<void> {
+  await testing.token.create({
+    symbol: symbol,
+    collateralAddress: address
+  })
+  await testing.generate(1)
+}
+async function mintTokens (testing: Testing, symbol: string, amount: number): Promise<void> {
+  await testing.token.mint({
+    symbol: symbol,
+    amount: amount
+  })
+  await testing.generate(1)
+}
+async function setCollateralToken (testing: Testing, symbol: string): Promise<void> {
+  await testing.rpc.loan.setCollateralToken({
+    token: symbol,
+    factor: new BigNumber(1),
+    fixedIntervalPriceId: `${symbol}/USD`
+  })
+  await testing.generate(1)
+}
+async function setLoanToken (testing: Testing, symbol: string): Promise<void> {
+  await testing.rpc.loan.setLoanToken({
+    symbol: symbol,
+    fixedIntervalPriceId: `${symbol}/USD`
+  })
+  await testing.generate(1)
+}
+async function createVault (testing: Testing, schemeId: string, address?: string): Promise<string> {
+  const vaultId = await testing.rpc.container.call(
+    'createvault', [address ?? await testing.generateAddress(), schemeId]
+  )
+  await testing.generate(1)
+  return vaultId
+}
+async function depositToVault (testing: Testing, vaultId: string, address: string, tokenAmt: string): Promise<void> {
+  await testing.rpc.container.call('deposittovault', [vaultId, address, tokenAmt])
+  await testing.generate(1)
+}
+async function takeLoan (testing: Testing, vaultId: string, amounts: string | string[]): Promise<void> {
+  await testing.rpc.container.call('takeloan', [{ vaultId, amounts }])
+  await testing.generate(1)
+}
