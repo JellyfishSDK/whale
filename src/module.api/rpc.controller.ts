bc89b6108c19f4db20b28f679c6e61068dc44391
Fuxing Loh <git@fuxing.dev>
2022-02-03 15:02:42 +0800

    expose JSON RPC via RpcController

--- src/module.api/rpc.controller.ts
+++ src/module.api/rpc.controller.ts
@@ -10,6 +10,9 @@ import {
   Post
 } from '@nestjs/common'
 import { JsonRpcClient } from '@defichain/jellyfish-api-jsonrpc'
+import { IsArray, IsIn, IsNotEmpty, IsOptional, IsString } from 'class-validator'
+import { ApiRawResponse } from '@src/module.api/_core/api.response'
+import { RpcApiError } from '@defichain/jellyfish-api-core'
 
 /**
  * MethodWhitelist is a whitelist validation pipe to check
@@ -17,7 +20,7 @@ import { JsonRpcClient } from '@defichain/jellyfish-api-jsonrpc'
  * Non whitelisted method call will result in a ForbiddenException.
  *
  * Direct access to DeFiD should not be allowed,
- * that could used used as an attack against DeFi whale services.
+ * that could be used as an attack against DeFi whale services.
  * (by changing our peers)
  */
 @Injectable()
@@ -37,11 +40,21 @@ export class MethodWhitelist implements PipeTransform {
   }
 }
 
-/**
- * Call Data Transfer Object
- */
-export class CallDto {
+export class JSONRPCParams {
   params?: any[]
+}
+
+export class JSONRPC {
+  @IsString()
+  @IsNotEmpty()
+  @IsIn(MethodWhitelist.methods, {
+    message: 'RPC method not whitelisted'
+  })
+  method!: string
+
+  @IsOptional()
+  @IsArray()
+  params!: any[]
 }
 
 @Controller('/rpc')
@@ -49,9 +62,33 @@ export class RpcController {
   constructor (private readonly client: JsonRpcClient) {
   }
 
+  @Post()
+  async post (@Body() rpc: JSONRPC): Promise<ApiRpcResponse> {
+    try {
+      const result = await this.client.call(rpc.method, rpc.params, 'lossless')
+      return new ApiRpcResponse(result)
+    } catch (err: any) {
+      if (err instanceof RpcApiError || err.payload !== undefined) {
+        return new ApiRpcResponse(null, err.payload)
+      }
+
+      throw err
+    }
+  }
+
   @Post('/:method')
   @HttpCode(200)
-  async call (@Param('method', MethodWhitelist) method: string, @Body() callDto?: CallDto): Promise<any> {
+  async call (@Param('method', MethodWhitelist) method: string, @Body() callDto?: JSONRPCParams): Promise<any> {
     return await this.client.call(method, callDto?.params ?? [], 'lossless')
+  }
+}
+
+class ApiRpcResponse extends ApiRawResponse {
+  constructor (
+    public readonly result: any | null,
+    public readonly error: any = null,
+    public readonly id: null = null
+  ) {
+    super()
   }
 }
