0fb79d340d4bc39f4b99537c9d01a8a59adb16ee
websterlcl <w.canonbrother@gmail.com>
2022-02-22 18:56:32 +0800

    convert human readable addr from placeauctionbid index

--- src/module.indexer/model/dftx/place.auction.bid.ts
+++ src/module.indexer/model/dftx/place.auction.bid.ts
@@ -1,10 +1,12 @@
 import { DfTxIndexer, DfTxTransaction } from '@src/module.indexer/model/dftx/_abstract'
 import { PlaceAuctionBid, CPlaceAuctionBid } from '@defichain/jellyfish-transaction'
 import { RawBlock } from '@src/module.indexer/model/_abstract'
-import { Injectable, Logger } from '@nestjs/common'
+import { Inject, Injectable, Logger } from '@nestjs/common'
 import { VaultAuctionHistoryMapper } from '@src/module.model/vault.auction.batch.history'
 import { toBuffer } from '@defichain/jellyfish-transaction/dist/script/_buffer'
 import { HexEncoder } from '@src/module.model/_hex.encoder'
+import { fromScriptHex } from '@defichain/jellyfish-address'
+import { NetworkName } from '@defichain/jellyfish-network'
 
 @Injectable()
 export class PlaceAuctionBidIndexer extends DfTxIndexer<PlaceAuctionBid> {
@@ -12,6 +14,7 @@ export class PlaceAuctionBidIndexer extends DfTxIndexer<PlaceAuctionBid> {
   private readonly logger = new Logger(PlaceAuctionBidIndexer.name)
 
   constructor (
+    @Inject('NETWORK') private readonly network: NetworkName,
     private readonly vaultAuctionHistoryMapper: VaultAuctionHistoryMapper
   ) {
     super()
@@ -19,6 +22,8 @@ export class PlaceAuctionBidIndexer extends DfTxIndexer<PlaceAuctionBid> {
 
   async indexTransaction (block: RawBlock, transaction: DfTxTransaction<PlaceAuctionBid>): Promise<void> {
     const data = transaction.dftx.data
+    const hex = toBuffer(data.from.stack).toString('hex')
+    const from = fromScriptHex(hex, this.network)?.address as string
 
     await this.vaultAuctionHistoryMapper.put({
       id: `${data.vaultId}-${data.index}-${transaction.txn.txid}`,
@@ -26,7 +31,7 @@ export class PlaceAuctionBidIndexer extends DfTxIndexer<PlaceAuctionBid> {
       sort: `${HexEncoder.encodeHeight(block.height)}-${transaction.txn.txid}`,
       vaultId: data.vaultId,
       index: data.index,
-      from: toBuffer(data.from.stack).toString('hex'),
+      from: from,
       amount: data.tokenAmount.amount.toString(),
       tokenId: data.tokenAmount.token,
       block: { hash: block.hash, height: block.height, medianTime: block.mediantime, time: block.time }
