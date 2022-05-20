56d63e02d4afd807fb61a63dc48921591b3c3dfa
websterlcl <w.canonbrother@gmail.com>
2022-05-20 18:43:17 +0800

    check height after 90 mins

--- src/module.defid/defid.probes.ts
+++ src/module.defid/defid.probes.ts
@@ -5,7 +5,11 @@ import { blockchain as bc } from '@defichain/jellyfish-api-core'
 
 @Injectable()
 export class DeFiDProbeIndicator extends ProbeIndicator {
-  constructor (private readonly client: JsonRpcClient) {
+  lastBlock: bc.Block<bc.Transaction> | undefined
+
+  constructor (
+    private readonly client: JsonRpcClient
+  ) {
     super()
   }
 
@@ -27,13 +31,16 @@ export class DeFiDProbeIndicator extends ProbeIndicator {
    * Readiness of DeFiD.
    * - defid is not in initial block download
    * - defid is connected to only count<5 peers
+   * - defid checks stale tip by checking coming block after 90 mins
    */
   async readiness (): Promise<HealthIndicatorResult> {
     let info: bc.BlockchainInfo
     let peers: number
+    let block: bc.Block<bc.Transaction>
     try {
       info = await this.client.blockchain.getBlockchainInfo()
       peers = await this.client.net.getConnectionCount()
+      block = await this.client.blockchain.getBlock(info.bestblockhash, 2)
     } catch (err) {
       return this.withDead('defid', 'unable to connect to defid')
     }
@@ -53,6 +60,19 @@ export class DeFiDProbeIndicator extends ProbeIndicator {
       return this.withDead('defid', 'defid is not connected to any peer', details)
     }
 
+    if (this.lastBlock === undefined) {
+      this.lastBlock = block
+    }
+
+    if (now() - this.lastBlock.time >= 90 * 60 && this.lastBlock.height >= block.height) {
+      return this.withDead('defid', 'defid chain is stale', details)
+    }
+    this.lastBlock = block
+
     return this.withAlive('defid', details)
   }
+}
+
+function now (): number {
+  return Math.floor(new Date().getTime() / 1000)
 }
